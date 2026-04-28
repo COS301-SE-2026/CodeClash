4035229f67bac41c431c9bbd3a695cc370af656f
kikoketchup <nsmzoneli@gmail.com>
2026-04-28 23:46:01 +0200

    chore: initialised and did docker setup and folder structure

---
+++ database/init.sql
@@ -0,0 +1,16 @@
+--very generic tables that can be changed later, just trying not to keep the file empty
+CREATE TABLE IF NOT EXISTS users (
+  user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  username VARCHAR(50) UNIQUE NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS elo_ratings (
+  elo_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id UUID REFERENCES users(user_id),
+  game_mode VARCHAR(20) NOT NULL,
+  rating INTEGER DEFAULT 600,
+  updated_at TIMESTAMP DEFAULT NOW()
+);
