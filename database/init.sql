48e02089aa3834a2a82ea056b91c57ff7f68072c
morganthegirlboss <u24910882@tuks.co.za>
2026-05-12 02:33:38 +0200

    removing function signature and equation in child tables, changed implementation so children tables of problems table function like children tables

--- database/init.sql
+++ database/init.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TYPE problem_category AS ENUM ('math', 'programming');
 
 CREATE TABLE IF NOT EXISTS problems (
-  problem_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  id SERIAL PRIMARY KEY,
   type problem_category NOT NULL,
   difficulty_level ENUM('Easy', 'Medium', 'Difficult') NOT NULL,
   title VARCHAR(20) NOT NULL,
@@ -24,29 +24,24 @@ CREATE TABLE IF NOT EXISTS elo_ratings (
   game_mode VARCHAR(20) NOT NULL,
   rating INTEGER DEFAULT 600,
   updated_at TIMESTAMP DEFAULT NOW(),
-  CONSTRAINT math_category_check CHECK (
-        (SELECT type FROM problems WHERE id = problem_id) = 'math'
-    )
+ 
 );
 
 
 CREATE TABLE IF NOT EXISTS math_problems (
-  problem_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  difficulty_level ENUM('Easy', 'Medium', 'Difficult') NOT NULL,
-  title VARCHAR(20) NOT NULL,
-  description VARCHAR(40) NOT NULL,
-  time_limit TIME(2) NOT NULL,
-  equation VARCHAR(20) NOT NULL,
-  solution_formula VARCHAR(20) NOT NULL
+  id SERIAL PRIMARY KEY,
+  problem_id INT NOT NULL REFERENCES problems(id) ON DELETE CASCADE,
+  --equation VARCHAR(20) NOT NULL,
+  solution_formula VARCHAR(20) NOT NULL,
+   CONSTRAINT math_category_check CHECK (
+        (SELECT type FROM problems WHERE id = problem_id) = 'math'
+    )
 );
 
 CREATE TABLE IF NOT EXISTS programming_problems (
-  problem_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  difficulty_level ENUM('Easy', 'Medium', 'Difficult') NOT NULL,
-  title VARCHAR(20) NOT NULL,
-  description VARCHAR(40) NOT NULL,
-  time_limit TIME(2) NOT NULL,
-  function_signature VARCHAR(25) NOT NULL,
+  id SERIAL PRIMARY KEY,
+  problem_id INT NOT NULL REFERENCES problems(id) ON DELETE CASCADE,
+  --function_signature VARCHAR(25) NOT NULL,
   supported_languages ENUM('java', 'c++') NOT NULL,
   CONSTRAINT programming_category_check CHECK (
         (SELECT type FROM problems WHERE id = problem_id) = 'programming'
