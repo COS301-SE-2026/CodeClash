95eafe52d331c936cd6a31170824bdf0d5debc56
morganthegirlboss <u24910882@tuks.co.za>
2026-05-12 02:19:23 +0200

    Adding virtual table and problem category type to be compatible with Diso's tables

--- database/init.sql
+++ database/init.sql
@@ -7,6 +7,17 @@ CREATE TABLE IF NOT EXISTS users (
   created_at TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TYPE problem_category AS ENUM ('math', 'programming');
+
+CREATE TABLE IF NOT EXISTS problems (
+  problem_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  type problem_category NOT NULL,
+  difficulty_level ENUM('Easy', 'Medium', 'Difficult') NOT NULL,
+  title VARCHAR(20) NOT NULL,
+  description VARCHAR(40) NOT NULL,
+  time_limit TIME(2) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS elo_ratings (
   elo_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   user_id UUID REFERENCES users(user_id),
@@ -14,6 +25,9 @@ CREATE TABLE IF NOT EXISTS elo_ratings (
   rating INTEGER DEFAULT 600,
   updated_at TIMESTAMP DEFAULT NOW()
 );
+
+
+//virtual table with category enum variable math or programming and then children tables have category=math or programming
 
 CREATE TABLE IF NOT EXISTS math_problems (
   problem_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -33,5 +47,4 @@ CREATE TABLE IF NOT EXISTS programming_problems (
   time_limit TIME(2) NOT NULL,
   function_signature VARCHAR(25) NOT NULL,
   supported_languages ENUM('java', 'c++') NOT NULL
-  // java, c++
 );
