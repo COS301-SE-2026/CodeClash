b6ea37c6ffe1b56a817120a614654501fa6b017f
swe-yy <swey@cos332.cs.up>
2026-05-14 12:39:05 +0200

    removed commented out code and unused imports

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import cors from 'cors'
 const app = express();
 app.disable('x-powered-by');    //so express version isn't included in responses
 
-app.use(cors());
+app.use(cors({origin: process.env.FRONTEND_URL || 'http://localhost:5173'}));
 app.use(express.json());
 
 app.get('/health', (req: Request, res: Response) => {
