08b58d4f03430b8d0d87a517ea1f48f2e6183462
TaskeenAbdoola <u22698681@tuks.co.za>
2026-05-14 15:58:07 +0200

    commiting to merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+import './App.css'*/
+
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
+  /*const [count, setCount] = useState(0)
 
   return (
     <>
@@ -116,7 +119,9 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  )*/
+
+  return <Login />;
 }
 
 export default App
