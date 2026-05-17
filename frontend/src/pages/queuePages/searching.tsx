d369af81a5d3ed0881ce56b062bba4e1076bfab4
morganthegirlboss <u24910882@tuks.co.za>
2026-05-17 19:06:46 +0200

    circle for user

---
+++ frontend/src/pages/queuePages/searching.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './searching.css';
+
+
+
+//create in searching page, when found immediately go to found, that is what can be in interface for searching
+
+
+const Searching = () => {
+  return (
+    <div className="page-container">
+      <div className="searching-section">
+       <div className="purple-circle"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Searching;
