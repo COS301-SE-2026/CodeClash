557bd336d2baac0c072a11d049775e70b646bfcf
morganthegirlboss <u24910882@tuks.co.za>
2026-05-17 19:04:36 +0200

    base for searching page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/global.css'
+import Searching from './pages/queuePages/searching' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-        <App/>
+    <Searching/>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
