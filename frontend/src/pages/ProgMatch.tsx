8aeba09fa2797997a3a62d65002a470adf3d007f
Swe-yy <u24584585@tuks.co.za>
2026-05-19 12:48:42 +0200

    page components without styling

--- frontend/src/pages/ProgMatch.tsx
+++ frontend/src/pages/ProgMatch.tsx
@@ -1,24 +1,84 @@
-import type React from "react"
+import React, { useState } from "react";
 import { Editor } from "@monaco-editor/react";
 
-interface ProgMatchProps{
+interface ProgMatchProps {
     language: string;
 }
 
-const ProgMatch: React.FC<ProgMatchProps> = ({language}) => {
+
+const ProgMatch: React.FC<ProgMatchProps> = ({ language }) => {
+
+    const [input, setInput] = useState("");
+    function handleChange(value: any) {
+        setInput(value);
+        console.log(value);
+
+
+    }
+
 
     return (
         <div className="fixed inset-0 flex flex-col ">
-            <div className="bg-gray-300 text-center p-8 m-20 h-20">Life Bars will go here</div>
+            <div className="bg-gray-300 text-center h-20">Life Bars will go here</div>
 
-            <div className="flex flex-row flex-1 w-full h-full justify-evenly">
-                <div className="flex items-center">
-                    <Editor height="70vh" width="70vh" defaultLanguage={language} defaultValue="// Your code here"  />
+            <div className="flex flex-row flex-1 w-full h-full justify-between items-center">
+                <div className="flex flex-col items-center">
+                    {/* User and question difficulty */}
+                    <div className="flex justify-between w-full">
+                        <p>User 1's Challenge</p>
+                        <div>
+                            {/* this div will need to be changed to the ui component we use for difficulty tags */}
+                            Easy
+                        </div>
+                    </div>
+                    {/* Question - this will also be replaced by a question div ui component*/}
+                    <div>
+                        {/* This will be replaced by a question ui component  */}
+                        <h1> &lt; / &gt; Coding Question</h1>
+                        <p>Players will have to write or evaluate some code</p>
+
+                        <div>
+                            Some questions will have given code and ask players to provide the correct output. <br></br>
+                            Other question will give a small scenario and ask players to write the code that will solve some problem
+                        </div>
+
+                    </div >
+
+                    <Editor
+                        height="40vh"
+                        width="70vh"
+                        defaultLanguage={language}
+                        defaultValue="// Your code here"
+                        onChange={handleChange}
+                        onMount={(editor, monaco) => {
+                            monaco.editor.defineTheme('default', {
+                                base: 'vs',
+                                'inherits': true,
+                                rules: [
+                                    {
+                                        token: "identifier",
+                                        foreground: '#9CDCFE'
+                                    },
+                                    {
+                                        token: "type",
+                                        foreground: "#1AAFB0"
+                                    },
+                                ],
+                                colors: {
+                                    'editor.background': '#a7a7a7',
+                                }
+                            });
+                            monaco.editor.setTheme('default')
+                        }}
+
+                    />
+
+
                 </div>
                 <div>Progress bar</div>
             </div>
-           
-            
+
+
         </div >
     )
 }
