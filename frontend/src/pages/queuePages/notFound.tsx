eea3f23a85bb537e5e3745e22e2d0148b29fac16
morganthegirlboss <u24910882@tuks.co.za>
2026-05-19 03:51:37 +0200

    text for not found page 2

---
+++ frontend/src/pages/queuePages/notFound.tsx
@@ -0,0 +1,22 @@
+import './notFound.css';
+
+const notFound = () => {
+  return (
+    <div className="page-container">
+      <div className="searching-section">
+
+        <div className="notFound-text">Opponent Not Found!</div>
+        <div className="searching-text">Please select one of the following</div>
+
+        <div className="row-button">
+          <button className="cancel-button">Decline Match</button>
+          <button className="approve-button">Accept Match</button>
+          </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default notFound;
