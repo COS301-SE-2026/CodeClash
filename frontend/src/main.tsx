1dd32e7919921c32d12a702ce8dc1c9e02f79192
morganthegirlboss <u24910882@tuks.co.za>
2026-05-19 13:03:38 +0200

    updating my branch

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,11 @@
+
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/global.css'
-import Searching from './pages/queuePages/searching' 
-import Found from './pages/queuePages/found' 
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <Found/>
-    </BrowserRouter>
-  </React.StrictMode>
+  <StrictMode>
+    <App/>
+  </StrictMode>
 )
