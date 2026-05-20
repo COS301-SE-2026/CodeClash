5baf0e4a061d84090213c75e194b01b68516d594
Swe-yy <u24584585@tuks.co.za>
2026-05-20 16:33:21 +0200

    Started server for websocket in the backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,30 @@
 import app from './app'
+import WebSocketServer from 'ws'
 
 const PORT = process.env.PORT || 3000;
+const WEBSOCKET_PORT = Number(process.env.WEBSOCKET_PORT) || 7000;
 
+// WebSocket
+const socketserver = new WebSocketServer.Server({ port: WEBSOCKET_PORT });
+socketserver.on('connection', (socket) => {
+  socket.on('message', (data) => {
+    // we will have to decide on a data format so that we can parse this and dispatch 
+
+    // we will also decide on response codes/types
+    socket.send("response");
+  });
+
+  socket.on('error', (error) => {
+    // will need error handling
+  });
+
+  socket.on('open', () => {
+    socket.send("ready");
+  })
+})
+
+
+// REST API
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
