6d4cab7b0dd4655459bedc02d8e01955d4701783
Swe-yy <u24584585@tuks.co.za>
2026-05-21 13:45:23 +0200

    updated vitest.config for backend

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
      globals: true,
-     exclude: ['**/node_modules/**', '**/tests/unit/api/**' ]
+     exclude: ['**/node_modules/**', '**/tests/unit/api/**' ],
+
+     coverage: {
+        provider: 'v8',
+        reporter: ['text','lcov'],
+        include: ['src/**'],
+        exclude: ['src/tests/**']
+     }
     }
 })
