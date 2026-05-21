e82bd0715f7b4e5cfb5c01b38dae051f40b02f56
morganthegirlboss <u24910882@tuks.co.za>
2026-05-21 20:23:45 +0200

    more cases for event emitter

--- backend/src/events/matchmaking.events.ts
+++ backend/src/events/matchmaking.events.ts
@@ -6,4 +6,9 @@ import UserDto, { MatchDto } from "../Matchmaking Service/matchmaking.dto";
 export interface MatchmakingEvents {
   "player:joined": (user: UserDto) => void; //player entered queue
   "player:left": (userId: number, queue: string) => void;
+
+  "match:searching": (user: UserDto) => void; //matchmaking function ran but found no opponent yet
+  "match:found": (player1Id: number, player2Id: number) => void; //two players were found to be paired of r a match
+  "match:created": (match: MatchDto) => void;
+
 }
