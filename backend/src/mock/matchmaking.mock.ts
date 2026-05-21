fa5dc2ddb7d8e975ef5df412a6610eca3b71b98f
morganthegirlboss <u24910882@tuks.co.za>
2026-05-21 20:26:57 +0200

    starting on creating mock data to be used for the demo

---
+++ backend/src/mock/matchmaking.mock.ts
@@ -0,0 +1,17 @@
+import UserDto from "../Matchmaking Service/matchmaking.dto";
+
+
+ 
+export interface MockUser {
+  id: number;
+  username: string;
+  elo: number;
+}
+ 
+export const MOCK_USERS: MockUser[] = [
+  { id: 1, username: "morgan", elo: 400 },  
+  { id: 2, username: "diso", elo: 900 },
+  { id: 3, username: "ntu", elo: 800  },  
+  { id: 4, username: "swey", elo: 1200 },
+  { id: 5, username: "taskeen", elo: 1250  },  
+];
