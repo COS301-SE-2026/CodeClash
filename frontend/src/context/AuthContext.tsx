0a985bfd1d6cf69cdbe1bd69739f3310fdedcfc1
Swelihle Makhathini <u24584585@tuks.co.za>
2026-05-22 01:22:54 +0200

    Console logs
    removed debugging 
    
    Signed-off-by: Swelihle Makhathini <u24584585@tuks.co.za>

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -46,17 +46,12 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
   const clearError = useCallback(() => setError(null), [])
 
   const signIn = useCallback(async (email: string, password: string) => {
-    console.log("logging in user: ", email);
     setError(null)
     try {
      const amp =  await amplifySignIn({ username: email, password })
 
-     console.log(amp);
-
       const cognitoUser = await getCurrentUser()
 
-      console.log(cognitoUser);
-      
       setUser({
         username: cognitoUser.username,
         userId: cognitoUser.userId,
