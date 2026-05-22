3c4669c0d0de856d77b53b5752c6795d835b63cf
Swe-yy <u24584585@tuks.co.za>
2026-05-22 02:20:24 +0200

    fixed finish game

--- frontend/src/pages/ProgMatch.tsx
+++ frontend/src/pages/ProgMatch.tsx
@@ -19,28 +19,24 @@ interface ProgMatchProps {
 
 
 const ProgMatch: React.FC<ProgMatchProps> = ({ language, back }) => {
-
-    // LifeBar 
-
     const [player_1_life, set_player_1_life] = useState(90);
     const [player_2_life, set_player_2_life] = useState(100);
 
     function handleChange(value: any) {
-        const answered = [...input]
+        const answered = [...input];
         answered[q_index] = value;
 
         set_input(answered);
     }
-
 
     // Countdown
     const [duration, set_duration] = useState<number>(5);
 
     const expiryTime = () => {
         const time = new Date();
-        time.setSeconds(time.getSeconds() + (duration * 60));
+        time.setSeconds(time.getSeconds() + duration * 60);
         return time;
-    }
+    };
 
     const { seconds, minutes } = useTimer({ expiryTimestamp: expiryTime() });
 
@@ -51,7 +47,9 @@ const ProgMatch: React.FC<ProgMatchProps> = ({ language, back }) => {
     // questions
     const [questions, set_questions] = useState<QuestionDTO[]>();
     const [q_index, set_q_index] = useState(0);
-    const [difficulty, set_difficulty] = useState<'easy' | 'medium' | 'hard'>('easy');
+    const [difficulty, set_difficulty] = useState<"easy" | "medium" | "hard">(
+        "easy",
+    );
     const [title, set_title] = useState("");
     const [question, set_question] = useState("");
     const [description, set_description] = useState("");
@@ -66,18 +64,15 @@ const ProgMatch: React.FC<ProgMatchProps> = ({ language, back }) => {
         set_title(q_1.title);
         set_question(q_1.question);
         set_description(q_1.description ?? "");
-
-    }, [])
+    }, []);
 
     function updateQuestion(q_idx: number) {
-        if (typeof questions !== 'undefined') {
-
-            if (q_idx >= questions.length) {
-                set_player_1_done(true);
+        if (typeof questions !== "undefined") {
+            if (q_idx >= questions.length) { 
+                set_player_1_done(true); 
                 set_q_index(questions.length - 1);
             }
-            else if (q_idx < 0)
-                return
+            else if (q_idx < 0) return;
             else {
                 const q = questions[q_idx];
                 set_q_index(q_idx);
@@ -87,11 +82,8 @@ const ProgMatch: React.FC<ProgMatchProps> = ({ language, back }) => {
                 set_description(q.description ?? "");
 
                 // clear editor
-                editorRef.current?.setValue(input[q_idx] ?? default_value)
-
+                editorRef.current?.setValue(input[q_idx] ?? default_value);
             }
-
-
         }
     }
 
@@ -102,61 +94,71 @@ const ProgMatch: React.FC<ProgMatchProps> = ({ language, back }) => {
     const [player_2_done, set_player_2_done] = useState(false);
 
     useEffect(() => {
-        set_player_1_progress((q_index / (questions?.length ?? 1)) * 100)
-    }, [q_index])
+        set_player_1_progress((q_index / (questions?.length ?? 1)) * 100);
+    }, [q_index]);
 
     // Data sent to backend - NOT CONNECTED RIGHT NOW
     function submit() {
-
-        // answered all questions 
+        // answered all questions
     }
 
-    // user input 
+    // user input
     const [input, set_input] = useState<string[]>(Array(questions?.length ?? 0));
-
 
     return (
         <div className="fixed inset-0 flex flex-rowjustify-evenly">
             <div className="flex flex-col w-[80%] m-2 justify-between">
-                <Button active={true} onClick={back} className="w-[15%] absolute left-5" variant={'outline'} >Exit</Button>
+                <Button
+                    active={true}
+                    onClick={back}
+                    className="w-[15%] absolute left-5"
+                    variant={"outline"}
+                >
+                    Exit
+                </Button>
                 {/* header */}
                 <div className="flex w-full h-[20%] justify-between items-center m-1 p-2">
-
                     {/* Player 1 Progress */}
                     <div className="w-[40%] flex flex-col items-start h-[60%] justify-center self-end ">
-                        <Progress className=" w-full h-7 shadow-[0_4px_6px_rgba(0,0,0,0.3)]"
+                        <Progress
+                            className=" w-full h-7 shadow-[0_4px_6px_rgba(0,0,0,0.3)]"
                             value={player_1_life}
                             progress_colour="#5f5980"
                         ></Progress>
                         <div className="flex w-[50%] h-[60%] items-center m-2">
-                            <img src={blue_avatar} alt="user one avatar" className="h-[120%] flex items-center"></img>
-                            <Badge className="bg-white badge-font" >
-                                User 1
-                            </Badge>
+                            <img
+                                src={blue_avatar}
+                                alt="user one avatar"
+                                className="h-[120%] flex items-center"
+                            ></img>
+                            <Badge className="bg-white badge-font">User 1</Badge>
                         </div>
-
                     </div>
 
                     {/* Clock */}
-                    <div className="w-[15%] h-20 flex items-center justify-center text-4xl font-semibold border-b-1 border-t-1 rounded-2xl" >
+                    <div className="w-[15%] h-20 flex items-center justify-center text-4xl font-semibold border-b-1 border-t-1 rounded-2xl">
                         <span>
-                            {String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}
+                            {String(minutes).padStart(2, "0")}:
+                            {String(seconds).padStart(2, "0")}
                         </span>
                     </div>
 
                     {/* Player 2 Progress */}
                     <div className="w-[40%] flex flex-col items-end h-[60%] justify-center self-end ">
-                        <Progress className=" w-full h-7 scale-x-[-1] shadow-[0_4px_6px_rgba(0,0,0,0.3)]"
+                        <Progress
+                            className=" w-full h-7 scale-x-[-1] shadow-[0_4px_6px_rgba(0,0,0,0.3)]"
                             value={player_2_life}
-                            progress_colour="#5f5980"></Progress>
+                            progress_colour="#5f5980"
+                        ></Progress>
                         <div className="flex w-[50%] h-[60%] items-center justify-end m-2">
-                            <Badge className="bg-white badge-font" >
-                                User 2
-                            </Badge>
-                            <img src={puprle_avatar} alt="user one avatar" className="h-[120%] flex items-center "></img>
+                            <Badge className="bg-white badge-font">User 2</Badge>
+                            <img
+                                src={puprle_avatar}
+                                alt="user one avatar"
+                                className="h-[120%] flex items-center "
+                            ></img>
                         </div>
                     </div>
-
                 </div>
 
                 {/* Question */}
@@ -176,38 +178,53 @@ const ProgMatch: React.FC<ProgMatchProps> = ({ language, back }) => {
                             defaultValue={default_value}
                             onChange={handleChange}
                             onMount={(editor: any, monaco: any) => {
-                                editorRef.current = editor
-                                monaco.editor.defineTheme('default', {
-                                    base: 'vs',
-                                    'inherits': true,
+                                editorRef.current = editor;
+                                monaco.editor.defineTheme("default", {
+                                    base: "vs",
+                                    inherits: true,
                                     rules: [
                                         {
                                             token: "identifier",
-                                            foreground: '#000000'
+                                            foreground: "#000000",
                                         },
                                         {
                                             token: "type",
-                                            foreground: "#1AAFB0"
+                                            foreground: "#1AAFB0",
                                         },
                                     ],
                                     colors: {
-                                        'editor.background': '#c4c4c4',
-                                    }
+                                        "editor.background": "#c4c4c4",
+                                    },
                                 });
-                                monaco.editor.setTheme('default');
-                                editor.updateOptions({
-                                })
+                                monaco.editor.setTheme("default");
+                                editor.updateOptions({});
                             }}
-
                         />
                     </div>
                     <div className="mt-5  flex justify-center w-[95%]">
-                        {q_index > 0 && <Button active={true} className="w-[15%] absolute left-5" variant={'outline'} onClick={() => updateQuestion(q_index - 1)}>Back</Button>}
-                        <Button active={(input[q_index]?.trim().length ?? 0) > 0 && input[q_index]?.trim() !== '// Your code here'} className="w-[20%] flex self-center" onClick={() => updateQuestion(q_index + 1)}>Submit</Button>
+                        {q_index > 0 && (
+                            <Button
+                                active={true}
+                                className="w-[15%] absolute left-5"
+                                variant={"outline"}
+                                onClick={() => updateQuestion(q_index - 1)}
+                            >
+                                Back
+                            </Button>
+                        )}
+                        <Button
+                            active={
+                                (input[q_index]?.trim().length ?? 0) > 0 &&
+                                input[q_index]?.trim() !== "// Your code here"
+                            }
+                            className="w-[20%] flex self-center"
+                            onClick={() => updateQuestion(q_index + 1)}
+                        >
+                            Submit
+                        </Button>
                     </div>
-
                 </Question>
-            </div >
+            </div>
 
             <MatchProgress
                 questions={questions ?? []}
@@ -218,9 +235,8 @@ const ProgMatch: React.FC<ProgMatchProps> = ({ language, back }) => {
                 done={player_1_done}
                 opponent_done={player_2_done}
             ></MatchProgress>
-
         </div>
-    )
-}
+    );
+};
 
 export default ProgMatch;
