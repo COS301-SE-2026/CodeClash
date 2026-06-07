cb74ab2d2a5a98d4a305bf43a842f9dfe34d6f77
morganthegirlboss <u24910882@tuks.co.za>
2026-06-07 23:01:05 +0200

    setting up verification

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,13 +3,16 @@ import express from 'express';
 import {createServer} from 'http';
 import {WebSocketServer} from 'ws';
 import {fetchAuthSession} from 'aws-amplify/auth'
+import {CognitoJwtVerifier} from 'aws-jwt-verify'
 
+const userPoolID = process.env.COGNITO_USER_POOL_ID;
 
-//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = createServer(app);
 
 const wss = new WebSocketServer({ noServer : true}); //makes websocket not create its own http server but just use server created above
+
 
 
 // app.listen(PORT, () => {
