bf7bc8906b46f7f2c7d85cc7e5c9c05be56d4e91
morganthegirlboss <u24910882@tuks.co.za>
2026-06-07 23:14:44 +0200

    setting up verification payload that comes after verifying token (jwt)for user to check if they've been authenticated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,12 @@ const wss = new WebSocketServer({ noServer : true}); //makes websocket not creat
     userPoolId: `${process.env.COGNITO_USER_POOL_ID}`,
     tokenUse: "id",
     clientId: `${process.env.COGNITO_CLIENT_ID}`,
-  })
+  });
+
+  export interface authPayload{
+    userId: number;
+    username: string;
+  }
 
 
 // app.listen(PORT, () => {
