b49cba8ec87ffd7b4110c01c1a14d4df8bca74ce
morganthegirlboss <u24910882@tuks.co.za>
2026-06-08 13:26:22 +0200

    starting on ws server function

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,16 +6,18 @@ import {fetchAuthSession} from 'aws-amplify/auth'
 
 const PORT = process.env.PORT || 3000;
 
+
+export const WSServer = () => {
+
 const server = createServer(app);
 
 const wss = new WebSocketServer({noServer : true}) //makes websocket not create its own http server but just use server created above
 
 
-wss.on("connection", async (ws : WebSocket) =>{
+wss.on("connection", async (ws : WebSocket, req) =>{
 
-    try{
-        
-    }
+    const url = new URL(req.url!, `http://${req.headers.host}`);
+    const token = url.searchParams.get("token");
 
 
 }
@@ -24,5 +26,5 @@ wss.on("connection", async (ws : WebSocket) =>{
 
 
 
-
 )
+}
