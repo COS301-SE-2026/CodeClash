926357716b6e7aea2aa4156b621704f54b090991
morganthegirlboss <u24910882@tuks.co.za>
2026-06-08 13:14:54 +0200

    separating concerns finished

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,3 +4,25 @@ import {createServer} from 'http';
 import {WebSocketServer} from 'ws';
 import {fetchAuthSession} from 'aws-amplify/auth'
 
+const PORT = process.env.PORT || 3000;
+
+const server = createServer(app);
+
+const wss = new WebSocketServer({noServer : true}) //makes websocket not create its own http server but just use server created above
+
+
+wss.on("connection", async (ws : WebSocket) =>{
+
+    try{
+        
+    }
+
+
+}
+
+
+
+
+
+
+)
