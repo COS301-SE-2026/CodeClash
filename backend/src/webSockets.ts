1d4735be9018abbc4238e80aaf40f4b624b44ff4
morganthegirlboss <u24910882@tuks.co.za>
2026-06-17 06:41:29 +0200

    starting on logic for creating and handling list of client web socket connections

--- backend/src/webSockets.ts
+++ backend/src/webSockets.ts
@@ -1,3 +1,7 @@
 import WebSocket from "ws";
 
 const clientList = new Map<WebSocket, string>();
+
+
+export const registerConnection = (ws : WebSocket, userId : string) =>
+    clientList.set(ws,userId);
