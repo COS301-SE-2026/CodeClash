d6ddf63eed16a6bacc071d26bbf045313ca5c4d4
morganthegirlboss <u24910882@tuks.co.za>
2026-06-17 07:13:42 +0200

    logic for the redis client

--- backend/redis-client.ts
+++ backend/redis-client.ts
@@ -1,6 +1,9 @@
-import Redis from "ioredis";
+// import Redis from "ioredis";
+import {createClient} from "redis";
+
+const redisClient = createClient({url : process.env.FRONTEND_URL || 'http://localhost:5173'}); //i am not sure if this is the correct url, must come back
 
 // redis instance
-const redis = new Redis();
+// const redis = new Redis();
 
-export default redis;
+// export default redis;
