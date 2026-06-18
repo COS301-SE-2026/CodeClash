f2cf58f07e68e19ce772207c632d4cc4a2a5e39a
TaskeenAbdoola <u22698681@tuks.co.za>
2026-06-18 16:30:17 +0200

    Added the background image and specific radial- gradient

--- frontend/src/pages/Welcome.tsx
+++ frontend/src/pages/Welcome.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import helloRobot from '../assets/HelloRobot_Pink.png';
+import symbolBackground from '../assets/SymbolBackground.png';
 
 interface WelcomeProps {
   onSignIn?: () => void;
@@ -8,7 +9,12 @@ interface WelcomeProps {
 
 const Welcome: React.FC<WelcomeProps> = ({ onSignIn, onSignUp }) => {
   return (
-    <div className="bg-background min-h-screen w-full flex items-center overflow-hidden px-[8%]">
+    <div className="min-h-screen w-full flex items-center overflow-hidden px-[8%]"
+      style={{ background: 'radial-gradient(circle at 88% 88%, #B91551 0%, #850F3B 20%, #630B3C 30%, #530A24 38%)'}}>
+
+        {/*Background frame*/}
+        <img src={symbolBackground} alt="" className="absolute inset-0 w-full h-full object-cover opacity-100 pointer-events-none"
+        />
 
       {/* For the left column */}
       <div className=" flex flex-col justify-center gap-6 w-1/2">
@@ -26,7 +32,7 @@ const Welcome: React.FC<WelcomeProps> = ({ onSignIn, onSignUp }) => {
         className="
           w-[60%] rounded-xl bg-button-primary text-button-text-primary text-md font-extrabold py-4
           hover:scale-105 transition duration-200 ease-in-out"
-        onClick={onSignIn}
+        onClick={onSignUp}
         type="button"
       >
         Sign up
@@ -48,9 +54,9 @@ const Welcome: React.FC<WelcomeProps> = ({ onSignIn, onSignUp }) => {
 
       {/* For the right column (robot)*/}
       <div className="w-1/2 flex items-center justify-center">    
-        <img src={helloRobot} alt="CodeClash Robot Avatar" className="w-[85%] h-auto object-contain"/>
+        <img src={helloRobot} alt="CodeClash Robot Avatar" className="w-[105%] h-auto object-contain mix-blend-screen -translate-x-10 translate-y-18"/>
       </div>
-      
+
     </div>
   );
 };
