25b1163c44ad4040555ab675428bcd729b663924
TaskeenAbdoola <u22698681@tuks.co.za>
2026-06-18 16:29:44 +0200

    Fixed this as it was giving a blank screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,5 @@
 import React from'react';
 import ReactDOM from 'react-dom/client'
-//import { RouterProvider } from 'react-router-dom'
-//import { router } from './routes'
 import App from './App'
 import { AuthProvider } from './context/AuthContext'
 import './amplify-config'
@@ -10,7 +8,8 @@ import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-    </RouterProvider>
+    <AuthProvider>
+      <App/>
+    </AuthProvider>
   </React.StrictMode>,
 )
