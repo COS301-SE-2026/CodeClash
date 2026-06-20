48694b2881ec251679fca3cd185c7b3301f30ca3
morganthegirlboss <u24910882@tuks.co.za>
2026-06-20 20:33:27 +0200

    starting logic for message sent if client successfully joins queue

--- backend/src/wsMessageHandler.ts
+++ backend/src/wsMessageHandler.ts
@@ -2,6 +2,7 @@ import WebSocket from "ws";
 import UserDto from "./Matchmaking Service/matchmaking.dto";
 import {dequeue, enqueue} from "./Matchmaking Service/matchmaking.service"
 import { removeConnection } from "./wsClients";
+import redis from 'redis';
 
 export const handleMessage = async(ws: WebSocket, data: string, client: UserDto) => {
     let message: any;
@@ -10,6 +11,11 @@ export const handleMessage = async(ws: WebSocket, data: string, client: UserDto)
         message = JSON.parse(data);
     }
     catch{
-        ws.send(JSON.stringify({type: 'ERROR', message: 'Invalid JSON Data'}))
+        ws.send(JSON.stringify({type: 'ERROR', message: 'Invalid JSON Data'}));
+        return;
+    }
+
+    if(message.type === "JOIN_QUEUE"){
+        await enqueue(client, client.game_mode);
     }
 }
