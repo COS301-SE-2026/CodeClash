4e643da3a0a3dcaea6175d37d344c81dee52ceb0
morganthegirlboss <u24910882@tuks.co.za>
2026-06-20 20:21:22 +0200

    starting on logic for message handling with web sockets

--- backend/src/wsMessageHandler.ts
+++ backend/src/wsMessageHandler.ts
@@ -2,3 +2,7 @@ import WebSocket from "ws";
 import UserDto from "./Matchmaking Service/matchmaking.dto";
 import {dequeue, enqueue} from "./Matchmaking Service/matchmaking.service"
 import { removeConnection } from "./wsClients";
+
+export const handleMessage = async(ws: WebSocket, data: string, client: UserDto) => {
+    
+}
