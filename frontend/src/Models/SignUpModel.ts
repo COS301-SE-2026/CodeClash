7d03a20d6edaed50958c7de21cc03f0368a0de25
TaskeenAbdoola <u22698681@tuks.co.za>
2026-06-22 23:01:40 +0200

    Starting the model for signup

---
+++ frontend/src/Models/SignUpModel.ts
@@ -0,0 +1,14 @@
+export interface SignUpForm {
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string; 
+    password: string;
+    acceptedTerms: boolean;
+}
+
+export interface SignUpContent {
+    title: string;
+    tagline: string;
+}
+
