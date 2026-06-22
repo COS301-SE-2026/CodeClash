52daf11ea2f850b6f8e45f3c3a79103549125a92
TaskeenAbdoola <u22698681@tuks.co.za>
2026-06-22 23:26:11 +0200

    Entering the contents for the interface

--- frontend/src/Models/SignUpModel.ts
+++ frontend/src/Models/SignUpModel.ts
@@ -10,5 +10,14 @@ export interface SignUpForm {
 export interface SignUpContent {
     title: string;
     tagline: string;
+    confirmTitle: string,
+    confirmTagline: (email: string) => string;
+}
+
+export const signUpContent: SignUpContent = {
+    title: 'Create Your Account',
+    tagline: 'Build your skills. Earn your rank.',
+    confirmTitle: 'Please verify your email',
+    confirmTagline: (email: string) => 'Enter the code sent to ${email}',
 }
 
