59bf51a182134912617c4c299fd496eb4465e95d
Ntuuu <mbatha05n@gmail.com>
2026-06-25 19:26:55 +0200

    added: basic config definition in backend config file and commented out the husky pre commit script before its done

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,1 +1,19 @@
-import js from "@eslint/js";
+import base from "../eslint.config.base.js"
+import { defineConfig, gloablgnores } from "eslint/config"
+
+export default defineConfig([
+  ...base,
+  globalgnores(["node_modules, "dist", "coverage"]),
+  {
+    files: ["**/*.ts"],
+    rules: {
+        "no-console" : "off",
+      },
+  },
+    {
+      files: ["**/*.test.ts", "**/*.spec.ts"],
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+      },
+  },
+]);
