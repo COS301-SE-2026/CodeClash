9da0d8f83df71d11f88857abb6acc261a70fd824
morganthegirlboss <u24910882@tuks.co.za>
2026-06-30 15:04:39 +0200

    starting on creating background for popup page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,15 @@ import App from './App'
 import { AuthProvider } from './context/AuthContext'
 import './amplify-config'
 import './styles/global.css'
-import Profile from './pages/Profile'
+import { Popup } from './pages/Popup'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       {/* <Profile/> */}
-      <App/> 
+      {/* <App/>  */}
+      <Popup/>
     </AuthProvider>
   </React.StrictMode>,
 )
