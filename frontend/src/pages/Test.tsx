f74eb57adc87c67c03e072aa0aafb4857bcdbdf8
morganthegirlboss <u24910882@tuks.co.za>
2026-06-30 19:26:13 +0200

    got popup sizing to work

--- frontend/src/pages/Test.tsx
+++ frontend/src/pages/Test.tsx
@@ -5,11 +5,11 @@ import React from "react"
 const Test: React.FC = () => {
 
     return(
-        <div className="min-h-screen w-full bg-[var(--secondary)] flex items-center justify-center p-6 bg-[url(./robot.png)] bg-center bg-no-repeat bg-size-[auto_700px]">
-            <div className="size-1/3">
-                <img src='./robot.png' alt="robot" className="relative w-full z-0 inset-0"></img>
+        <div className="min-h-screen w-full bg-[var(--secondary)] flex items-center justify-center p-6 bg-[url(./robot.png)] bg-center bg-no-repeat bg-size-[auto_800px]">
+            <div className="relative w-1/3">
+                {/* <img src='./robot.png' alt="robot" className="relative w-full z-0 inset-0"></img> */}
 
-                <div className="bg-[var(--secondary)] border rounded-3xl pt-20 pb-8 px-8 text-center shadow-sm top-8 z-10">
+                <div className="bg-[var(--secondary)] w-full min-h-10 border rounded-3xl pt-12 pb-8 text-center shadow-sm absolute -top-9 h-110">
 
                 </div>
             </div>
