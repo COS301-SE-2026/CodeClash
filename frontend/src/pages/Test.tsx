3ae9b9cdd80fff6bd677160418ad4b5ae37b8dd8
morganthegirlboss <u24910882@tuks.co.za>
2026-06-30 19:09:58 +0200

    changing robot man to be apart of background image

--- frontend/src/pages/Test.tsx
+++ frontend/src/pages/Test.tsx
@@ -5,9 +5,13 @@ import React from "react"
 const Test: React.FC = () => {
 
     return(
-        <div className="min-h-screen w-full bg-[var(--secondary)] flex items-center justify-center p-6">
+        <div className="min-h-screen w-full bg-[var(--secondary)] flex items-center justify-center p-6 bg-[url(./robot.png)] bg-center bg-no-repeat bg-size-[auto_700px]">
             <div className="size-1/3">
-                <img src='./robot.png' alt="robot" className="w-full"></img>
+                <img src='./robot.png' alt="robot" className="relative w-full z-0 inset-0"></img>
+
+                <div className="bg-[var(--secondary)] border rounded-3xl pt-20 pb-8 px-8 text-center shadow-sm top-8 z-10">
+
+                </div>
             </div>
         </div>
     );
