c0c7250da2496d9d846dda70db5675a9f96e5bee
morganthegirlboss <u24910882@tuks.co.za>
2026-07-01 02:25:41 +0200

    trying to change branches

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,16 +4,12 @@ import App from './App'
 import { AuthProvider } from './context/AuthContext'
 import './amplify-config'
 import './styles/global.css'
-import Profile from './pages/Profile'
-import Popup from './pages/Popup'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      {/* <App/>  */}
-      {/* <Profile/> */}
-      <Popup/>
+      <App/> 
     </AuthProvider>
   </React.StrictMode>,
 )
