b9c121d03a6deb27e9a9efce5952068653fef5a1
morganthegirlboss <u24910882@tuks.co.za>
2026-07-02 19:04:36 +0200

    startin on corrected syntax for keyboard listener for selecting programming in popup.tsx

--- frontend/src/pages/Popup.tsx
+++ frontend/src/pages/Popup.tsx
@@ -18,9 +18,15 @@ const Popup: React.FC<PopupProps> = ({onMath, onProg, onCancel}) => {
         };
 
         window.addEventListener('keydown', math);
-        
+
         return () => window.removeEventListener('keydown', math);
-    }, [onMath])
+    }, [onMath]);
+    useEffect(() => {
+        const prog = (e: KeyboardEvent) => {
+            const alt = e.altKey;
+            
+        }
+    })
     return(
         <div className="min-h-screen w-full bg-[var(--secondary)] flex items-center justify-center p-6 bg-[url(./robot.png)] bg-center bg-no-repeat bg-size-[auto_800px]">
             <div className="relative w-xl">
@@ -34,18 +40,12 @@ const Popup: React.FC<PopupProps> = ({onMath, onProg, onCancel}) => {
                 
                 <div className="grid grid-flow-col grid-cols-2 gap-4 ml-8 mr-8 mt-15 h-45">
                     <div className="bg-[var(--secondary)] rounded-3xl text-center text-[var(--secondary-text)] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] mr-2 transition-all duration-200 hover:bg-rose-200 hover:-translate-y-px active:translate-y-0" 
-                        onClick={onMath}
-                        onKeyDown={(e) => {
-                            const alt = e.altKey;
-                            if(alt && e.key === 'L'){
-                                onMath?.();
-                            }
-                        }}
-                        >
+                        onClick={onMath}>
                         <h1 className="text-[32px] text-[var(--secondary-text)] font-[var(--heading)] font-bold mt-5">+ -</h1>
                         <h2 className="text-[32px] text-[var(--secondary-text)] font-[var(--heading)] font-bold mt-3">Math</h2>
                         </div>
-                    <div className="bg-[var(--secondary)] rounded-3xl text-center text-[var(--secondary-text)] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] ml-2 transition-all duration-200 hover:bg-rose-200 hover:-translate-y-px active:translate-y-0" onClick={onProg}>
+                    <div className="bg-[var(--secondary)] rounded-3xl text-center text-[var(--secondary-text)] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] ml-2 transition-all duration-200 hover:bg-rose-200 hover:-translate-y-px active:translate-y-0" 
+                        onClick={onProg}>
                         <h1 className="text-[32px] text-[var(--secondary-text)] font-[var(--heading)] font-bold mt-5">{"</>"}</h1>
                         <h2 className="text-[32px] text-[var(--secondary-text)] font-[var(--heading)] font-bold mt-3">Programming</h2>
                     </div>
