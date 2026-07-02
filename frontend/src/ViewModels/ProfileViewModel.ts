bc336323dde4c9da55cbc3b24b392f262899590a
morganthegirlboss <u24910882@tuks.co.za>
2026-07-02 16:20:59 +0200

    trying to fix sonarqube

--- frontend/src/ViewModels/ProfileViewModel.ts
+++ frontend/src/ViewModels/ProfileViewModel.ts
@@ -6,25 +6,19 @@ export interface ProfileViewModelProps{
     onBack? : () => void;
 }
 
-interface ProfileViewModel{
-    details: ProfileDetails;
-    isLoading: boolean;
-    displayError: string | null;
+// interface ProfileViewModel{
+//     details: ProfileDetails;
+//     isLoading: boolean;
+//     displayError: string | null;
     
-    handleBack: () => void; //takes user to dashboard
+//     handleBack: () => void; //takes user to dashboard
 
-}
+// }
 
 
-export function ProfileViewModelFunction(userId: string){
+// export function ProfileViewModelFunction(userId: string){
 
-    const [details, setDetails] = useState<ProfileDetails>(ProfileData);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
-
-    const loadProfile = useCallback(async () =>{
-        setIsLoading(true);
-        setError(null); 
-    })
-
-}
+//     const [details, setDetails] = useState<ProfileDetails>(ProfileData);
+//     const [isLoading, setIsLoading] = useState(true);
+//     const [error, setError] = useState<string | null>(null);
+// }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,6 @@ import { AuthProvider } from './context/AuthContext'
 import './amplify-config'
 import './styles/global.css'
 import { BrowserRouter } from 'react-router-dom'
-import Popup from './pages/Popup'
-import Profile from './pages/Profile'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
