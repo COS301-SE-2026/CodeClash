d82ab86717d89ad75371773c3a4fc3ee84b05385
Swe-yy <u24584585@tuks.co.za>
2026-07-03 21:18:51 +0200

    pulled from styling-update

--- frontend/src/ViewModels/SignUpViewModel.ts
+++ frontend/src/ViewModels/SignUpViewModel.ts
@@ -2,11 +2,8 @@ import { useCallback, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { signUpContent, formData, validateSignUpForm } from "../Models/SignUpModel";
 import type { SignUpContent, SignUpForm } from "../Models/SignUpModel";
+import { useNavigate } from "react-router-dom";
 
-export interface SignUpViewModelProps {
-    onSignIn?: () => void;
-    onBack?: () => void;
-}
 
 interface SignUpViewModel {
     content: SignUpContent;
@@ -24,9 +21,7 @@ interface SignUpViewModel {
     handleResend: () => Promise<void>;
 }
 
-export function SignUpViewModelFunction (
-    {onSignIn, onBack, }:
-    SignUpViewModelProps): SignUpViewModel {
+export function SignUpViewModelFunction (): SignUpViewModel {
 
         const { signUp, confirmSignUp, resendSignUpCode, error, clearError, isLoading } = useAuth();
 
@@ -35,6 +30,7 @@ export function SignUpViewModelFunction (
         const [needsConfirmation, setNeedsConfirmation] = useState(false);
         const [localError, setLocalError] = useState<string | null>(null);
         const [resendMessage, setResendMessage] = useState<string | null>(null);
+        const navigation = useNavigate();
 
         const setField = useCallback ((field: keyof SignUpForm, value: string | boolean) =>
         {
@@ -73,9 +69,9 @@ export function SignUpViewModelFunction (
             }
             try {
                 await confirmSignUp (form.username.trim(), confirmationCode.trim()); //If the validation is passed, Amplify will be called
-                onSignIn?.(); //If confirmation is successful, go to the SignIn page if it exists
+                navigation('/dashboard')
             } catch {}
-        }, [confirmationCode, form.username, confirmSignUp, clearError, onSignIn]); //Dependency array
+        }, [confirmationCode, form.username, confirmSignUp, clearError]); //Dependency array
 
         const handleResend = useCallback(async () =>
         {
