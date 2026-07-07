dca10c153574abf295066e52c6b3db2396d12c54
Swe-yy <u24584585@tuks.co.za>
2026-07-07 18:26:21 +0200

    found is working now with updated pop up styling

--- frontend/src/Views/Popup.tsx
+++ frontend/src/Views/Popup.tsx
@@ -1,7 +1,7 @@
 import '../styles/global.css'
 import React from "react"
 import { Card } from '@/components/ui/card'
-
+import robot from '../assets/Robots/arms_up.png'
 import { type PopupProps } from '../Models/PopUpModel';
 import { useSelectTopic } from '../ViewModels/PopUpViewModel';
 import { useNavigate } from 'react-router-dom';
@@ -21,7 +21,7 @@ const Popup: React.FC<PopupProps> = ({ isOpen, onClose }) => {
 
     return (
         <div className="fixed inset-0 z-50  bg-black/50 flex items-center justify-center  ">
-            {/* <img src={robot} alt='robot-background' className='absolute h-[80%] top-0' /> */}
+            <img src={robot} alt='robot-background' className='absolute h-[80%] top-0' />
             <div className="relative w-[50%] h-[4rem] top-[-6rem] ">
 
                 <Card className="bg-secondary h-[35rem] w-[100%] rounded-3xl  text-center flex items-center absolute">
