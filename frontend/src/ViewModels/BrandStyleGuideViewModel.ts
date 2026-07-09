41e59ababc002dcb57a0daf2ebb787abb826c34a
TaskeenAbdoola <u22698681@tuks.co.za>
2026-07-09 18:52:53 +0200

    function for the active section in the nav bar

--- frontend/src/ViewModels/BrandStyleGuideViewModel.ts
+++ frontend/src/ViewModels/BrandStyleGuideViewModel.ts
@@ -35,3 +35,37 @@ export const navSections = [
 ] as const;
 
 export type SectionId = typeof navSections[number]['id'];
+
+interface BrandStyleGuideViewModel {
+    content: BrandStyleGuideContent;
+    active: SectionId;
+    copied: string | null;
+    sectionScroll: (id: SectionId) => void;
+    clipboardCopy: (text: string, key: string) => void;
+}
+
+export function BrandStyleGuideViewModelFunction(): BrandStyleGuideViewModel {
+    const [active, setActive] = useState<SectionId>('intro');
+    const [copied, setCopied] = useState<string | null>(null);
+
+    useEffect(() => {
+        const handler = () => {
+            const scrollY = window.scrollY + 100; //how far the user has scrolled
+            const sections = navSections.map(n => ({ id: n.id, el: document.getElementById(n.id)})).filter(s => s.el !== null); //This maps every item in navSections, copies the id and looks for an HTML el with that id. Then, filter keeps only objects whose els exist.
+
+            for(let i = sections.length - 1; i >= 0; i--) { //start at the bottom and work up. if we start at the top then the section was passed already and will be True (wrong)
+                if(sections[i].el!.offsetTop <= scrollY) { //ofsetTop means how far the section is from the top of the page and this if statement tells us if we have scrolled here yet - it tells us where the user currently is
+                    setActive(sections[i].id as SectionId); //depending on where the user is, setActive changes and automatically highlights
+                    break;
+                }
+            }
+        };
+        window.addEventListener('scroll', handler, {passive: true}); //whenever the user scrolls, the handler function will run and the scroll event will only read information, not stop the scrolling
+        return () => window.removeEventListener('scroll', handler); //remove the listener when the scrolling stops otherwise the listener will keep on activating, wasting memory
+    }, []); //empty dependency array
+
+    return {
+        content: brandStyleGuideContent,
+        active,
+    }
+}
