7fa7ac6a7a76da801d62bcfdc1430fc76190cab4
TaskeenAbdoola <u22698681@tuks.co.za>
2026-07-10 13:24:46 +0200

    Rendering the view into react entry point

--- frontend/src/brand-style-guide-main.tsx
+++ frontend/src/brand-style-guide-main.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 
-import IntroSection from "./Views/BrandStyleGuide/IntroductionSection";
-import { brandStyleGuideContent } from "./Models/BrandStyleGuideModel";
+import BrandStyleGuide from './Views/BrandStyleGuide';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <div className="max-w-[860px] mx-auto px-6 py-12 bg-white min-h-screen">
-            <IntroSection content={brandStyleGuideContent}/>
-        </div>
+        <BrandStyleGuide/>
     </React.StrictMode>
 );
