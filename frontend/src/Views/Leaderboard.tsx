af6c655b757b637014d413aa4f66f3907a1948ef
morganthegirlboss <u24910882@tuks.co.za>
2026-07-10 17:33:50 +0200

    changing positioning of 1st, 2nd and 3rd

--- frontend/src/Views/Leaderboard.tsx
+++ frontend/src/Views/Leaderboard.tsx
@@ -42,13 +42,13 @@ const Leaderboard = (
     // </div>
     <div className="w-full min-h-screen bg-[#C0395A]">
         <Link to="/dashboard" className="text-[#530A24] font-[var(--font)] text-[32px] font-semibold ml-5">← Back</Link>
-        <h1 className="text-[64px] text-[#FFDBE4] text-center mt-3 mr-160 font-[var(--font)] font-semibold drop-shadow-[4px_4px_0px_rgba(0,0,0,0.25)]">Leader Board</h1>
+        <h1 className="text-[64px] text-[#FFDBE4] text-center mt-3 mr-215 font-[var(--font)] font-semibold drop-shadow-[4px_4px_0px_rgba(0,0,0,0.25)]">Leader Board</h1>
         
         <div className="flex justify-between items-start gap-10">
             <div className="w-2/3 flex flex-col gap-6">
             <div className="grid grid-cols-3 gap-6 items-end">
-            <div className="w-[239px] h-[304px] rounded-[20px] bg-[#FAC8D0] border-4 border-[#F4A0B2] drop-shadow-[10px_10px_6px_rgba(0,0,0,0.25)] flex flex-col items-center justify-center"></div>
-            <div style={{backgroundImage: `url(${winner})`}} className="w-[524px] h-[524px] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center"></div>
+            <div className="ml-7 mb-33 w-[239px] h-[304px] rounded-[20px] bg-[#FAC8D0] border-4 border-[#F4A0B2] drop-shadow-[10px_10px_6px_rgba(0,0,0,0.25)] flex flex-col items-center justify-center"></div>
+            <div style={{backgroundImage: `url(${winner})`}} className="w-[524px] h-[524px] -ml-40 bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center"></div>
             <div className="w-[239px] h-[304px] rounded-[20px] bg-[#FAC8D0] border-4 border-[#F4A0B2] drop-shadow-[-10px_10px_6px_rgba(0,0,0,0.25)] flex flex-col items-center justify-center"></div>
             </div>
             </div>
