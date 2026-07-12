02843cf6ea5ce5d090c08eb09ef48b0dea40ea28
morganthegirlboss <u24910882@tuks.co.za>
2026-07-12 23:49:43 +0200

    added image to 4th card

--- frontend/src/Views/Leaderboard.tsx
+++ frontend/src/Views/Leaderboard.tsx
@@ -67,7 +67,7 @@ const Leaderboard = (
                 <div className="text-[32px] text-[#C0395A] text-center font-[var(--font)] font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">ELO</div>
             </div>
             <div className="mb-40 -mt-40 ml-10 w-[239px] h-[77px] rounded-[20px] bg-[#FAC8D0] border-4 border-[#F4A0B2] drop-shadow-[10px_10px_6px_rgba(0,0,0,0.25)]">
-                <div style={{backgroundImage: `url(${profile})`}} className="w-[60px] h-[60px] bg-no-repeat"></div>
+                <div style={{backgroundImage: `url(${profile})`}} className="mt-1.5 w-[60px] h-[60px] bg-no-repeat rounded-full object-cover bg-size-[auto_120px] bg-[right_-30px_top_-30px]"></div>
             </div>
             <div className="mb-40 -mt-40 ml-75 w-[239px] h-[77px] rounded-[20px] bg-[#FAC8D0] border-4 border-[#F4A0B2] drop-shadow-[10px_10px_6px_rgba(0,0,0,0.25)]"></div>
             </div>
