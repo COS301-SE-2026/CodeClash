308631922f01d7d7792b1d92c641d4bfd03db44f
morganthegirlboss <u24910882@tuks.co.za>
2026-07-13 10:47:29 +0200

    added second row

--- frontend/src/Views/Guidebook.tsx
+++ frontend/src/Views/Guidebook.tsx
@@ -13,8 +13,9 @@ return(
         <div className="relative w-1/2 flex flex-col">
         <div className="grid grid-cols-2 grid-rows-4 gap-5 items-end">
             <div className="w-[160px] h-[160px] rounded-[20px] bg-[#FFD3DF] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] font-[var(--font-guidebook)] ml-25 mt-10">1</div>
-            <div className="w-[550px] h-[91px] bg-[#FFEFE0] opacity-[94%] rounded-[20px] border-[#F4A0B2] border-4 drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] mb-7 -ml-40"></div>
-            
+            <div className="w-[550px] h-[91px] bg-[#FFEFE0] opacity-[94%] rounded-[20px] border-[#F4A0B2] border-4 drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] mb-7.5 -ml-40"></div>
+            <div className="w-[160px] h-[160px] rounded-[20px] bg-[#FFD3DF] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] ml-25 -mt-5"></div>
+            <div className="w-[550px] h-[91px] bg-[#FFEFE0] opacity-[94%] rounded-[20px] border-[#F4A0B2] border-4 drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] mb-7.5 -ml-40"></div>
         </div>
         </div>
     </div>
