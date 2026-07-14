3e70bd4ceb7cb8cd9bed8b5295ee9b45770c955f
morganthegirlboss <u24910882@tuks.co.za>
2026-07-14 15:20:06 +0200

    figured out text positioning inside boxes

--- frontend/src/Views/Guidebook.tsx
+++ frontend/src/Views/Guidebook.tsx
@@ -11,8 +11,10 @@ return(
         <Link to="/dashboard" className="text-[#E8466E] font-[var(--font)] text-[32px] font-semibold ml-5">← Back</Link>
         <h1 className="text-[70px] text-[#E8466E] text-center font-[var(--font)] font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] -ml-235">Game Guide</h1>
         <div className="relative w-1/2 flex flex-col">
-        <div className="grid grid-cols-2 grid-rows-4 gap-5 items-end">
-            <div className="w-[160px] h-[160px] rounded-[20px] bg-[#FFD3DF] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] font-[var(--font-guidebook)] ml-25 mt-10">1</div>
+        <div className="grid grid-cols-2 grid-rows-4 gap-5 items-end relative">
+            <div className="ml-25 mt-10 w-[160px] h-[160px] rounded-[20px] bg-[#FFD3DF] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] relative">
+                <h1 className="font-silkscreen text-center text-[#E8466E] text-[64px] absolute top-6 left-14">1</h1>
+            </div>
             <div className="w-[550px] h-[91px] bg-[#FFEFE0] opacity-[94%] rounded-[20px] border-[#F4A0B2] border-4 drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] mb-8 -ml-40"></div>
             <div className="w-[160px] h-[160px] rounded-[20px] bg-[#FFD3DF] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] ml-25 mb-4"></div>
             <div className="w-[550px] h-[91px] bg-[#FFEFE0] opacity-[94%] rounded-[20px] border-[#F4A0B2] border-4 drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] mb-12 -ml-40"></div>
