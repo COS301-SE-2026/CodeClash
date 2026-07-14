6f9fd5d5f05bbefec59436244a05b4bd8fc4c380
morganthegirlboss <u24910882@tuks.co.za>
2026-07-14 15:24:25 +0200

    figured out text positioning inside stretched boxes

--- frontend/src/Views/Guidebook.tsx
+++ frontend/src/Views/Guidebook.tsx
@@ -13,9 +13,11 @@ return(
         <div className="relative w-1/2 flex flex-col">
         <div className="grid grid-cols-2 grid-rows-4 gap-5 items-end relative">
             <div className="ml-25 mt-10 w-[160px] h-[160px] rounded-[20px] bg-[#FFD3DF] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] relative">
-                <h1 className="font-silkscreen text-center text-[#E8466E] text-[64px] absolute top-6 left-14">1</h1>
+                <h1 className="font-silkscreen text-center text-[#E8466E] text-[64px] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] absolute top-6 left-14">1</h1>
             </div>
-            <div className="w-[550px] h-[91px] bg-[#FFEFE0] opacity-[94%] rounded-[20px] border-[#F4A0B2] border-4 drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] mb-8 -ml-40"></div>
+            <div className="relative w-[550px] h-[91px] bg-[#FFEFE0] opacity-[94%] rounded-[20px] border-[#F4A0B2] border-4 drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] mb-8 -ml-40">
+                <h1 className="text-[#E8466E] text-[24px] font-silkscreen drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] absolute top-5.5 left-32">Game Instruction 1</h1>
+            </div>
             <div className="w-[160px] h-[160px] rounded-[20px] bg-[#FFD3DF] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] ml-25 mb-4"></div>
             <div className="w-[550px] h-[91px] bg-[#FFEFE0] opacity-[94%] rounded-[20px] border-[#F4A0B2] border-4 drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] mb-12 -ml-40"></div>
             <div className="w-[160px] h-[160px] rounded-[20px] bg-[#FFD3DF] drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] ml-25 mb-6.5"></div>
