d5b9ef22dcda74df5904e207ee47825ca9e73360
Swe-yy <u24584585@tuks.co.za>
2026-07-15 23:48:01 +0200

    updated found view model testing

--- frontend/test/unit/ViewModels/FoundViewModel.test.tsx
+++ frontend/test/unit/ViewModels/FoundViewModel.test.tsx
@@ -2,12 +2,28 @@
 import { describe, vi } from "vitest";
 import { useFound } from "src/ViewModels/FoundViewModel";
 import { useSocket } from "src/context/Socket/hooks/useSocket";
-import { useMatchmakingSocket, matchAccepted, matchDeclined } from "src/context/Socket/hooks/useMatchmakingSocket";
+import { useMatchmakingSocket, matchAccepted, matchDeclined, joinMatchQueue } from "src/context/Socket/hooks/useMatchmakingSocket";
 import { renderHook } from "@testing-library/react";
 import { act } from "react";
+import type { Socket } from "socket.io-client";
 
-vi.mock('src/context/Socket/hooks/useSocket');
-vi.mock('src/context/Socket/hooks/useMatchmakingSocket')
+vi.mock('src/context/Socket/hooks/useSocket', () => ({
+    useSocket: vi.fn()
+}));
+
+vi.mock('src/context/Socket/hooks/useMatchmakingSocket', () => ({
+    useMatchmakingSocket: vi.fn(),
+    joinMatchQueue: vi.fn(),
+    matchAccepted: vi.fn(),
+    matchDeclined: vi.fn(),
+}))
+
+vi.mock('src/context/User/hooks/useUser', () => ({
+    useUser: () => {
+        return { elo: 600 }
+    },
+
+}))
 
 const mock_nav = vi.fn();
 vi.mock('react-router-dom', () => ({
@@ -17,22 +33,36 @@ vi.mock('react-router-dom', () => ({
 const handlers = new Map<string, Function>();
 
 describe("Testing found view model", () => {
-
-
-    const mock_socket = {
-        on: vi.fn((event: string, callback: Function) => {
+    const mock_socket: Partial<Socket> = {
+        on: vi.fn((event: string, callback: Function): Socket => {
             handlers.set(event, callback);
+            return mock_socket as Socket;
         }),
         off: vi.fn(),
         emit: vi.fn()
     }
 
 
-    vi.mocked(useSocket).mockReturnValue({ socket: mock_socket } as any);
-    vi.mocked(useMatchmakingSocket).mockReturnValue({
-        game_mode: 'prog',
-        pair_id: "12345ABCDE"
-    } as any);
+    beforeEach(() => {
+        vi.clearAllMocks();
+        vi.mocked(useSocket).mockReturnValue({ socket: mock_socket as Socket, isConnected: true });
+        vi.mocked(useMatchmakingSocket).mockReturnValue({
+            game_mode: 'math',
+            pair_id: "12345ABCDE"
+        } as any);
+
+        handlers.clear();
+    })
+
+
+    it("Verifies listeners are registered correctly", () => {
+        renderHook(() => useFound());
+
+        expect(mock_socket.on).toHaveBeenCalledWith('game_ready', expect.any(Function));
+        expect(mock_socket.on).toHaveBeenCalledWith('decline_done', expect.any(Function));
+        expect(mock_socket.on).toHaveBeenCalledWith('game_declined', expect.any(Function));  
+    })
+
 
     it("Tests decline for matched users", () => {
         const { result } = renderHook(() => useFound());
@@ -67,12 +97,12 @@ describe("Testing found view model", () => {
         })
 
         expect(result.current.loading).toBe(false);
-        expect(mock_nav).toHaveBeenCalledWith('prog-match');
+        expect(mock_nav).toHaveBeenCalledWith('math-match');
     })
 
-    it("Tests gameDecline navigation for the declining user", () => {
-
+    it("Tests declineGame navigation for the declining user", () => {
         
+
     })
 
 })
