f04488d7ec26f847f204e847af350b7a0c8627e4
TaskeenAbdoola <u22698681@tuks.co.za>
2026-07-18 00:31:05 +0200

    tests for calling onCancel and onConfirm

--- frontend/src/ViewModels/tests/ConfirmationViewModel.test.ts
+++ frontend/src/ViewModels/tests/ConfirmationViewModel.test.ts
@@ -15,4 +15,35 @@ describe("ConfirmationViewModel", () => {
 
         expect(result.current.isVisible).toBe(true);
     });
+
+    it("calls onConfirm when confirmed", () => {
+        const confirm = vi.fn();
+        const {result} = renderHook(()=>
+            ConfirmationViewModelFunction({
+                onConfirm: confirm,
+            })
+        );
+        act(() => {
+            result.current.showConfirm();
+            result.current. handleConfirm();
+        });
+
+        expect(confirm).toHaveBeenCalledOnce();
+    });
+
+    it("calls onCancel when cancelled", ()=> {
+        const cancel = vi.fn();
+        const {result} = renderHook(()=>
+            ConfirmationViewModelFunction({
+                onConfirm: vi.fn(),
+                onCancel: cancel,
+            })
+        );
+        act(()=> {
+            result.current.showConfirm();
+            result.current.handleCancel();
+        });
+
+        expect(cancel).toHaveBeenCalledOnce();
+    });
 })
