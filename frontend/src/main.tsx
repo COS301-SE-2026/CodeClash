bc7a183a760249a7c266e4e1ce6b540a5953dec3
Swe-yy <u24584585@tuks.co.za>
2026-07-20 15:45:29 +0200

    fixed main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,18 +5,13 @@ import { AuthProvider } from './context/AuthContext'
 import './amplify-config'
 import './styles/global.css'
 import { BrowserRouter } from 'react-router-dom'
-import Popup from './Views/Popup'
-import Leaderboard from './Views/Leaderboard'
-import Guidebook from './Views/Guidebook'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
 
   <AuthProvider>
     <BrowserRouter>
-      {/* <App /> */}
-      {/* <Leaderboard/> */}
-      <Guidebook/>
+      <App />
     </BrowserRouter>
   </AuthProvider>
 
