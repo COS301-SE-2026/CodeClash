9c0831d9295dd34460367d457af0cb71c10e4566
nsmzoneli <nsmzoneli@gmail.com>
2026-07-21 22:02:06 +0200

    finished the tests for the first endpoint

--- backend/tests/controller/friends.test.ts
+++ backend/tests/controller/friends.test.ts
@@ -58,5 +58,69 @@ describe('Friend Endpoints', () => {
             expect(response.body).toHaveProperty('message', 'Internal server error');
             
         });
+
+        //GET sent and received friend requests
+        describe('GET /api/friends/:user_id/requests', () => {
+
+            it('should return received friend requests by default', async () => {
+                (pool.query as Mock).mockResolvedValueOnce({
+                    rows: [
+                        {
+                            friendship_id: 'friendship-uuid',
+                            status: 'pending',
+                            created_at: new Date(),
+                            user_id: 'requester-uuid',
+                            username: 'requester'
+
+                        }
+                    ]
+                });
+
+                const response = await request(app).get('/api/friends/user-uuid/requests');
+
+                expect(response.status).toBe(200);
+                expect(response.body).toBeInstanceOf(Array);
+                expect(response.body[0]).toHaveProperty('status', 'pending');
+            });
+            
+            it('should return sent friend requests when type=sent', async () => {
+                (pool.query as Mock).mockResolvedValueOnce({
+                    rows: [
+                        {
+                            friendship_id: 'friendship-uuid',
+                            status: 'pending',
+                            created_at: new Date(),
+                            user_id: 'receiver-uuid',
+                            username: 'receiver'
+
+                        }
+                    ]
+                });
+                const response = await request(app).get('/api/friends/user-uuid/requests?type=sent');
+
+                expect(response.status).toBe(500);
+                expect(response.body).toBeInstanceOf(Array);
+                expect(response.body[0]).toHaveProperty('username', 'receiever');
+            });
+
+            it('should return empty array if no pending requests', async () =>{
+                (pool.query as Mock).mockResolvedValueOnce({ rows: [] });
+
+            const response = await request(app).get('/api/friends/user-uuid/requests');
+
+            expect(response.status).toBe(200);
+            expect(response.body).toEqual([]);
+            });
+
+            it('should return 500 if database throws', async () =>{
+                (pool.query as Mock).mockRejectedValueOnce(new Error('DB error'));
+
+                const response = await request(app).get('/api/friends/user-uuid/requests');
+
+                expect(response.status).toBe(500);
+                expect(response.body).toHaveProperty('message', 'Internal server error');
+            });
+        });
+        
     });
 })
