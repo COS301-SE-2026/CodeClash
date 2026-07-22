08c72c688cde049d586be582ec67bb3b9d424a3e
morganthegirlboss <u24910882@tuks.co.za>
2026-07-22 20:14:04 +0200

    creating new card component for game guide page

--- frontend/src/Views/Guidebook.tsx
+++ frontend/src/Views/Guidebook.tsx
@@ -12,7 +12,7 @@ return(
         <Link to="/dashboard" className="text-button-primary font-[var(--font)] text-[32px] font-semibold ml-5">← Back</Link>
         <h1 className="text-[80px] text-button-primary text-center justify-center font-[var(--font)] font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] -mt-5">Game Guide</h1>
         <GlassCard className="w-[90%] h-[80%] mx-auto">   
-        <div className="mt-4 ml-8 h-[47px] w-[870px] rounded-[20px] bg-blend-normal bg-[#FFEFE0] backdrop-blur-lg opacity-20"></div>
+        <GlassCard className="mt-4 ml-8 h-[10%] w-[20%]"></GlassCard>
         </GlassCard>
     </div>
 );
