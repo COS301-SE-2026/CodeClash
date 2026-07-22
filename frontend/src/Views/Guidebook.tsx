f67c2ac374daf8139f34791d1132bb31542a15d6
morganthegirlboss <u24910882@tuks.co.za>
2026-07-22 20:03:13 +0200

    adding blur background to game guide page

--- frontend/src/Views/Guidebook.tsx
+++ frontend/src/Views/Guidebook.tsx
@@ -8,10 +8,12 @@ const Guidebook = () => {
 
 
 return(
-    <div style={{backgroundImage: `url(${background})`}} className="w-full min-h-screen overflow-hidden bg-no-repeat object-cover bg-size-[auto_1200px] bg-[right_-20px_top_-70px]">
+    <div style={{backgroundImage: `url(${background})`}} className="w-full min-h-screen overflow-hidden bg-no-repeat object-cover bg-size-[auto_110%] bg-[right_40%_top_50%]">
         <Link to="/dashboard" className="text-button-primary font-[var(--font)] text-[32px] font-semibold ml-5">← Back</Link>
         <h1 className="text-[80px] text-button-primary text-center justify-center font-[var(--font)] font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] -mt-5">Game Guide</h1>
-      
+        <GlassCard className="w-[90%] h-[80%] mx-auto">   
+        <div className="mt-4 ml-8 h-[47px] w-[870px] rounded-[20px] bg-blend-normal bg-[#FFEFE0] backdrop-blur-lg opacity-20"></div>
+        </GlassCard>
     </div>
 );
 
