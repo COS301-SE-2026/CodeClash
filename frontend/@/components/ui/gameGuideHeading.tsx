b143d929135f0d1880c2ffe493aedf1923af7384
morganthegirlboss <u24910882@tuks.co.za>
2026-07-22 22:57:32 +0200

    continuing heading component for numbers in game guide

---
+++ frontend/@/components/ui/gameGuideHeading.tsx
@@ -0,0 +1,13 @@
+import type React from 'react';
+import { Card } from "./card";
+
+interface gameGuideHeadingProps{
+    children?: React.ReactNode
+    className?: string
+}
+
+const GameGuideHeading = ({children, classname} : GlassCardProps) => {
+    return(
+        <Card className={``}"></Card>
+    )
+}
