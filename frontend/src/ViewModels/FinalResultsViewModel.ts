c357219af280d704e18bf0fa504ab72c3c26b793
TaskeenAbdoola <u22698681@tuks.co.za>
2026-07-22 17:27:13 +0200

    Begininning useEffect - creating a loading animation

--- frontend/src/ViewModels/FinalResultsViewModel.ts
+++ frontend/src/ViewModels/FinalResultsViewModel.ts
@@ -16,3 +16,27 @@ interface FinalResultsViewModel {
     handlePlayAgain: () => void;
     handleReturn: () => void;
 }
+
+export function FinalResultsViewModelFunction ({
+    onPlayAgain, onReturn, fetchResults,
+}: FinalResultsViewModelProps): FinalResultsViewModel {
+    const [state, setState] = useState< 'loading' | 'results'>('loading');
+    const [loadingProgress, setLoadingProgress] = useState(0);
+    const [results, setResults] = useState<FinalResults[]>([]);
+    const [displayError, setDisplayError] = useState<string | null>(null);
+
+    useEffect(() => {
+        let progressInterval: ReturnType<typeof setInterval>;
+        let cancelled = false;
+
+        progressInterval = setInterval(() => { //create a fake loading animation that will gradually fill while waiting for backed. This is going to cont to UX cause otherwise they will just see a frozen loading screen
+            setLoadingProgress(prev => {
+                if (prev >= 90) { //this will have the bar slow down as it reaches 90% and wait for real data. This can be changed as it gets connected to backend
+                    clearInterval(progressInterval);
+                    return 90;
+                }
+                return prev + Math.random() * 8; //this will make the loading bar feel more natural instead of updating by the same amount the same time
+            });
+        }, 400);
+    })
+}
