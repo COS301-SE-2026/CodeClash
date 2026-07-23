801d691db3700e4aef525237a5292a4a1d827c59
Swe-yy <u24584585@tuks.co.za>
2026-07-23 20:54:37 +0200

    Match.ts sonar fixes

--- frontend/@/components/shared/Match.tsx
+++ frontend/@/components/shared/Match.tsx
@@ -105,11 +105,13 @@ export const MatchScreen: React.FC<MatchScreenProps> = ({
                             <div className='relative flex flex-row'>
                                 <img src={avatars[0]}
                                     className=" absolute w-20 h-30 object-cover left-20"
-                                    style={{ top: `${(question_number - 1 - current_question) * 9.6}rem` }}
+                                    style={{ top: `${(question_number - 1 - current_question) * 9.6}rem`}}
+                                     alt='progress avatar user 1'
                                 />
                                 <img src={avatars[0]}
                                     className=" absolute w-20 h-30 object-cover scale-x-[-1]"
                                     style={{ top: `${(question_number - 1 - opponent_progress) * 9.6}rem` }}
+                                     alt='progress avatar user 2'
                                 />
 
                             </div>
@@ -118,13 +120,14 @@ export const MatchScreen: React.FC<MatchScreenProps> = ({
                             <div className='relative  flex flex-col items-center justify-between h-[40rem]'>
                                 <div className="absolute bg-secondary h-[90%] w-[15%] -z-10 rounded-3xl "></div>
                                 {
-                                    [...Array(question_number)].map((q, id) => {
+                                    [...new Array(question_number)].map((_, id) => {
                                         return (
-                                            <React.Fragment key={id}>
+                                            <React.Fragment key={`${id}`}>
 
                                                 <div className=' w-[100%] h-[8rem] flex items-center justify-center col-start-2 '>
                                                     <img src={door}
                                                         className="w-20 h-20 object-cover rounded-full"
+                                                        alt='door'
                                                     />
                                                 </div>
                                             </React.Fragment>
