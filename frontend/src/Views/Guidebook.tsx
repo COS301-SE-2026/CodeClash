50a2684738329629e05ac7fb7ac921897b6d8b78
morganthegirlboss <u24910882@tuks.co.za>
2026-07-23 13:44:34 +0200

    added text in the third card of game guide

--- frontend/src/Views/Guidebook.tsx
+++ frontend/src/Views/Guidebook.tsx
@@ -17,39 +17,40 @@ return(
         <h1 className="text-[450%] text-button-primary text-center justify-center font-[var(--font)] font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] -mt-5">Game Guide</h1>
         <GlassCard className="w-[90%] h-[80%] mx-auto">
         <div className="flex grid grid-cols-4 gap-4">
-        <div className="flex grid grid-rows-6 gap-4 w-[100%] mt-[7%]"> 
-        <GameGuideCard className="w-[120%] h-[70%] ml-[8%]">
+        <div className="flex grid grid-rows-6 gap-7 w-[100%] mt-[17%]"> 
+        <GameGuideCard className="w-[120%] h-[80%] ml-[8%]">
             <GameGuideNumberCard className="w-[34%] h-[100%] ml-[1%] -mt-[5%]">
                <GameGuideHeading>1</GameGuideHeading>
             </GameGuideNumberCard>
-            <GameGuideCardText className="-ml-[60%] -mt-[6%] text-[75%]">First select "Play Now" on your Dashboard and choose what type of match you want to play</GameGuideCardText>
+            <GameGuideCardText className="-ml-[62%] -mt-[2%] text-[70%]">First select "Play Now" on your Dashboard and choose what type of match you want to play</GameGuideCardText>
         </GameGuideCard>
-        <GameGuideCard className="w-[120%] h-[70%] ml-[8%]">
+        <GameGuideCard className="w-[120%] h-[80%] ml-[8%]">
             <GameGuideNumberCard className="w-[34%] h-[100%] ml-[1%] -mt-[6.5%]">
                 <GameGuideHeading>2</GameGuideHeading>
             </GameGuideNumberCard>
-            <GameGuideCardText className="-ml-[60%] -mt-[9%] text-[60%]">Casual Play is for players who want to practise without impacting their ELO. Ranked Play is for players who want to put their skills to the test by battling against others (Be careful! This impacts your ELO!)"</GameGuideCardText>
+            <GameGuideCardText className="-ml-[62%] -mt-[8%] text-[62%]">Casual Play is for players who want to practise without impacting their ELO. Ranked Play is for players who want to put their skills to the test by battling against others (Be careful! This impacts your ELO!)"</GameGuideCardText>
         </GameGuideCard>
-        <GameGuideCard className="w-[120%] h-[70%] ml-[8%]">
+        <GameGuideCard className="w-[120%] h-[80%] ml-[8%]">
             <GameGuideNumberCard className="w-[34%] h-[100%] ml-[1%] -mt-[6.5%]"> 
                 <GameGuideHeading>3</GameGuideHeading>
             </GameGuideNumberCard>
+            <GameGuideCardText className="-ml-[62%] -mt-[7%] text-[70%]">After selecting Play Now, wait until we find someone for you to battle against. Your opponent's ELO will be similar to yours!</GameGuideCardText>
         </GameGuideCard>
-        <GameGuideCard className="w-[120%] h-[70%] ml-[8%]">
+        <GameGuideCard className="w-[120%] h-[80%] ml-[8%]">
             <GameGuideNumberCard className="w-[34%] h-[100%] ml-[1%] -mt-[6.5%]">
                 <GameGuideHeading>4</GameGuideHeading>
             </GameGuideNumberCard>
         </GameGuideCard>
-        <GameGuideCard className="w-[120%] h-[70%] ml-[8%]">
+        <GameGuideCard className="w-[120%] h-[80%] ml-[8%]">
             <GameGuideNumberCard className="w-[34%] h-[100%] ml-[1%] -mt-[6.5%]">
                 <GameGuideHeading>5</GameGuideHeading>
             </GameGuideNumberCard>
         </GameGuideCard>
-        <GameGuideCard className="w-[120%] h-[70%] ml-[8%]">
+        {/* <GameGuideCard className="w-[120%] h-[70%] ml-[8%]">
             <GameGuideNumberCard className="w-[34%] h-[100%] ml-[1%] -mt-[6.5%]">
                 <GameGuideHeading>6</GameGuideHeading>
             </GameGuideNumberCard>
-        </GameGuideCard>
+        </GameGuideCard> */}
         </div>
         </div>
         </GlassCard>
