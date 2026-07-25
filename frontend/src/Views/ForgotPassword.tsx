b2c1509fd4a6a8cb44819f04a2456f9e8f87619b
TaskeenAbdoola <u22698681@tuks.co.za>
2026-07-25 18:10:01 +0200

    chnaging comment

--- frontend/src/Views/ForgotPassword.tsx
+++ frontend/src/Views/ForgotPassword.tsx
@@ -3,7 +3,7 @@ import { ForgotPasswordViewModelFunction } from "../ViewModels/ForgotPasswordVie
 import type { ForgotPasswordViewModelProps } from "../ViewModels/ForgotPasswordViewModel";
 import { CheckCircle } from "lucide-react";
 
-{/*Copying the fields and button class from SignUp.tsx */}
+//Copying the fields and button class from SignUp.tsx
 const fieldClass = "fields w-[100%] max-w-[90vw] h-[3rem] bg-white rounded-lg px-[2%] border-[0.5px] border-primary outline-none transition-all duration-200 focus:border-pink-400 focus:shadow-[0_0_0_3px_rgba(185,21,81,0.15)] disabled:opacity-50 text-primary font-medium placeholder:text-primary/60 focus:text-primary";
 const buttonPrimaryClass = "w-[100%] max-w-[90vw] h-[3rem] text-[1.5rem] rounded-lg font-bold cursor-pointer flex items-center justify-center transition-all duration-200 hover:-translate-y-px active:translate-y-0 disabled:opacity-50 bg-button-primary text-button-text-primary shadow-badge";
 
