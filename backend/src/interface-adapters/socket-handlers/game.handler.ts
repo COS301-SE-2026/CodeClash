d45d32f1a0c8666f1a396fbf4dc45de839bca24a
nsmzoneli <nsmzoneli@gmail.com>
2026-07-25 21:29:31 +0200

    added submission handler skeleton, must verify structure first

--- backend/src/interface-adapters/socket-handlers/game.handler.ts
+++ backend/src/interface-adapters/socket-handlers/game.handler.ts
@@ -1,6 +1,14 @@
 //These handlers should handle the submission of answers and sending results of the game to both players
 import { Socket, Server } from "socket.io";
-import { gameService } from 'src/application/usecases/services/game.service';
-import { IQuestionRepository } from "src/application/interfaces/IQuestionRepository";
-import { IEloRepository} from "src/application/interfaces/IEloRepository";
-import { ISubmissionRepository } from "src/application/interfaces/ISubmissionRepository";
+import { CheckAnswer } from "src/application/usecases/check-answer";
+import { World } from "src/entities/World";
+import { PlayersComponent, LifeComponent } from "src/entities/components";
+
+//Handler to return a player's submission results to them
+export const submitAnswer = (async () => {
+
+    //send resut only to submitting player
+
+
+    //notify opponent of player's progress
+})
