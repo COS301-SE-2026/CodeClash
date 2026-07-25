ad55a8d758c155d4a98b876627628a733b3c6f96
TaskeenAbdoola <u22698681@tuks.co.za>
2026-07-25 17:45:58 +0200

    updated app.tsx to just handle simple routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import type React from "react";
-import {Route, Routes, useNavigate } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Dashboard from "./Views/Dashboard";
 import Layout from "./layout";
 import Welcome from "./Views/Welcome";
@@ -10,10 +10,8 @@ import Searching from "./pages/queuePages/searching";
 import Found from "./pages/queuePages/found";
 import MathMatch from "./Views/MathsMatch";
 import FinalResults from "./Views/FinalResults";
-import type { PlayerFinalResults } from "./Models/FinalResultsModel";
 
 const App: React.FC = () => {
-    const navigate = useNavigate();
     return (
         <Routes>
             <Route path='/' element={<Welcome/>}/>
@@ -26,12 +24,7 @@ const App: React.FC = () => {
             <Route path='/match' element={<MathMatch/>}/>
             {/* <Route path='/prog-match' element={<ProgMatch language="javascript"/>}/> */}
 
-            <Route path= '/results' element= {<FinalResults onReturn={() => navigate('/dashboard')} onPlayAgain={() => navigate('/dashboard')}
-                fetchResults={async (): Promise<PlayerFinalResults[]> => {
-                    {/*REPLACE WITH REAL BACKEND CALL*/}
-                    return [];
-                }}/>
-            }/>
+            <Route path= '/results' element= {<FinalResults/>}/>
 
             {/* Pages with sidebar inside the app */}
             <Route element={<Layout />}>
