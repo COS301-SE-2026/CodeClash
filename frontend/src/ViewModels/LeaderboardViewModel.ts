fe9a23d219b422acc34434a3b1de03975bf729f7
morganthegirlboss <u24910882@tuks.co.za>
2026-07-25 09:12:25 +0200

    working on Leaderboard View Model, fetching data

---
+++ frontend/src/ViewModels/LeaderboardViewModel.ts
@@ -0,0 +1,20 @@
+import {useNavigate} from 'react-router-dom'
+import { useState, useEffect, useCallback } from 'react'
+import { LeaderboardUserProps, LeaderboardProps, LeaderboardUserData, fetchLeaderboardUsers } from 'src/Models/LeaderboardModel'
+
+
+export function LeaderboardViewModel(){
+    const [userData, setUserData] = useState<LeaderboardUserProps[]>([]);
+    const [isLoadingData, setIsLoadingData] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    const loadLeaderboard = useCallback(async () => {
+        setIsLoadingData(true);
+        setError(null);
+        try{
+            const data = await fetchLeaderboardUsers();
+            setUserData(data);
+        }
+
+    })
+}
