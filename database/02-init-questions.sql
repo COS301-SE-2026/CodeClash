dadbbf53ea0e30e8bfb741eb89b6783b4dfad7a7
Swe-yy <u24584585@tuks.co.za>
2026-07-26 14:53:21 +0200

    fixed lines to NOSONAR works properly

--- database/02-init-questions.sql
+++ database/02-init-questions.sql
@@ -1,88 +1,13 @@
-INSERT INTO
-    questions(
-        game_mode,
-        difficulty,
-        title,
-        description,
-        time_limit
-    )
-VALUES
-    (
-        'maths',
-        2,
-        'Linear Equations',
-        'Solve the following system of equations:\n2x + 3y = 11\n4x - y = 7',
-        '00:02:00'
-    ),
-    --NOSONAR
-    (
-        'maths',
-        1,
-        'Evaluate Quadratic Function',
-        'If f(x) = 3x² - 5x + 2, find the value of f(-2).',
-        '00:01:00'
-    ),
-    --NOSONAR
-    (
-        'maths',
-        2,
-        'Algebraic Simplification',
-        'Simplify the expression:\n((x² - 4)(x + 1)) / ((x - 2)(x² - 1))',
-        '00:02:00'
-    ),
-    --NOSONAR
-    (
-        'maths',
-        2,
-        'Solve Quadratic Equation',
-        'Find the roots of the quadratic equation:\nx² - 7x + 10 = 0',
-        '00:02:00'
-    ),
-    --NOSONAR
-    (
-        'maths',
-        1,
-        'Arithmetic Sequence',
-        'Given that the first term of an arithmetic sequence is 5 and the common difference is 3, find the 15th term.',
-        '00:03:00'
-    ),
-    --NOSONAR
-    (
-        'maths',
-        3,
-        'Solve Exponential Equation',
-        'Solve for x:\n5^(x + 1) = 125',
-        '00:02:00'
-    ),
-    --NOSONAR
-    (
-        'maths',
-        3,
-        'Rectangle Area Problem',
-        'If a rectangle has a length of (2x + 1) units and a width of (x - 3) units, and its area is 20 square units, find the possible values of x.',
-        '00:04:00'
-    ),
-    --NOSONAR
-    (
-        'maths',
-        2,
-        'Equation of a Straight Line',
-        'Determine the equation of a line that passes through the point (3, -2) and has a slope of 1/2.',
-        '00:02:00'
-    ),
-    --NOSONAR
-    (
-        'maths',
-        3,
-        'Factor Cubic Polynomial',
-        'Factor the cubic polynomial:\nx³ - 2x² - 5x + 6',
-        '00:03:00'
-    ),
-    --NOSONAR
-    (
-        'maths',
-        2,
-        'Geometric Sequence Sum',
-        'Given a geometric sequence with the first term a = 4 and the common ratio r = 2, find the sum of the first 6 terms.',
-        '00:02:00'
-    ) --NOSONAR
+
+INSERT INTO questions(game_mode, difficulty,title,description,time_limit)
+VALUES 
+('maths',2,'Linear Equations','Solve the following system of equations:\n2x + 3y = 11\n4x - y = 7', '00:02:00'), --NOSONAR
+('maths',1,'Evaluate Quadratic Function','If f(x) = 3x² - 5x + 2, find the value of f(-2).', '00:01:00'),--NOSONAR
+('maths',2,'Algebraic Simplification','Simplify the expression:\n((x² - 4)(x + 1)) / ((x - 2)(x² - 1))', '00:02:00'),--NOSONAR
+('maths',2,'Solve Quadratic Equation','Find the roots of the quadratic equation:\nx² - 7x + 10 = 0', '00:02:00'),--NOSONAR
+('maths',1,'Arithmetic Sequence','Given that the first term of an arithmetic sequence is 5 and the common difference is 3, find the 15th term.', '00:03:00'),--NOSONAR
+('maths',3,'Solve Exponential Equation','Solve for x:\n5^(x + 1) = 125', '00:02:00'),--NOSONAR
+('maths',3,'Rectangle Area Problem','If a rectangle has a length of (2x + 1) units and a width of (x - 3) units, and its area is 20 square units, find the possible values of x.', '00:04:00'),--NOSONAR
+('maths',2,'Equation of a Straight Line','Determine the equation of a line that passes through the point (3, -2) and has a slope of 1/2.', '00:02:00'),--NOSONAR
+('maths',3,'Factor Cubic Polynomial','Factor the cubic polynomial:\nx³ - 2x² - 5x + 6', '00:03:00'),--NOSONAR
+('maths',2,'Geometric Sequence Sum','Given a geometric sequence with the first term a = 4 and the common ratio r = 2, find the sum of the first 6 terms.', '00:02:00')--NOSONAR
