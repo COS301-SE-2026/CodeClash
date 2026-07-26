d72d3d5e1c4690959e9136bea5a782692d1cb534
morganthegirlboss <u24910882@tuks.co.za>
2026-07-27 01:31:31 +0200

    adding backing glass card and rows/cols to new game guid

---
+++ frontend/src/Views/GameGuide.tsx
@@ -0,0 +1,29 @@
+import bg from "../../src/assets/Background/solar_system.jpg"
+import GlassCard from "@/components/shared/GlassCard";
+import "../../src/styles/global.css"
+
+const GameGuide = () => {
+
+
+    return(
+
+        <div style={{backgroundImage: `url(${bg})`}} className="w-full min-h-screen overflow-y-auto bg-no-repeat object-cover bg-no-repeat bg-size-[auto_200%]">
+
+            <div className="bg-[#000000]/20 mx-auto mt-[1%] w-[95%] h-[95%] rounded-[20px] backdrop-blur-sm border-10 border-[#530A24]">
+                
+                <div className="flex flex-col grid grid-cols-2 grid rows-2">
+                    
+                </div>
+            </div>
+
+        </div>
+    
+
+    );
+
+
+
+}
+
+
+export default GameGuide;
