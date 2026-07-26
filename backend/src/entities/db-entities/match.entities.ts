a347420da47c481711195f1545af79161e1db1f4
nsmzoneli <nsmzoneli@gmail.com>
2026-07-26 20:38:15 +0200

    added matchlog entity, need to add other dependencies

--- backend/src/entities/db-entities/match.entities.ts
+++ backend/src/entities/db-entities/match.entities.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
-
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Users } from './user.entities';
+import { Match } from "../ecs-entities";
 
 @Entity()
 export class MatchProblems{
@@ -7,4 +8,28 @@ export class MatchProblems{
     match_problems_id!: string
 
     
+}
+
+@Entity()
+export class MatchLog {
+    @PrimaryGeneratedColumn('uuid')
+    log_id!: string;
+
+    @OneToOne(() => Match)
+    @JoinColumn({ name: 'match_id' })
+    match!: Match;
+
+    @OneToOne(() => Users)
+    @JoinColumn({ name: 'winner_id' })
+    winner!: Users;
+
+    @OneToOne(() => Users)
+    @JoinColumn({ name: 'loser_id' })
+    loser!: Users;
+
+    @Column( { nullable: true })
+    elo_gained!: number | null;
+
+    @Column( { nullable: true })
+    elo_lost!: number | null;
 }
