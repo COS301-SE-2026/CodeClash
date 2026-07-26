f104ba860a66ca2afd024d95c038fa5a749c5278
morganthegirlboss <u24910882@tuks.co.za>
2026-07-27 01:44:20 +0200

    added title to new game guide

--- frontend/src/Views/GameGuide.tsx
+++ frontend/src/Views/GameGuide.tsx
@@ -10,11 +10,12 @@ const GameGuide = () => {
         <div style={{backgroundImage: `url(${bg})`}} className="w-full min-h-screen overflow-y-auto bg-no-repeat object-cover bg-no-repeat bg-size-[auto_200%]">
 
             <div className="bg-[#000000]/20 mx-auto mt-[1%] w-[95%] h-[95%] rounded-[20px] backdrop-blur-sm border-10 border-[#530A24]">
-                
+                <h1 className="font-font font-bold text-secondary text-[230%] mx-auto text-center mt-[3%]">CODECLASH - Game Guide</h1>
                 <div className="flex flex-col grid grid-cols-2 grid rows-2">
-                    
+    
                 </div>
             </div>
+
 
         </div>
     
