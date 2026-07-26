be6979c2aa556455777fb83706ac283298557fad
TaskeenAbdoola <u22698681@tuks.co.za>
2026-07-26 23:04:35 +0200

    adding interface for per row and some mock data for testing purposes

--- frontend/src/Models/MatchHistoryModel.ts
+++ frontend/src/Models/MatchHistoryModel.ts
@@ -16,3 +16,36 @@ export interface MatchDetails {
     date: string;
     time: string;
 }
+
+export interface MatchRow {
+    id: string;
+    mode: MatchMode;
+    type: MatchType;
+    timestamp: string;
+    result: MatchResult;
+    details: MatchDetails;
+}
+
+export const mockedMatch: MatchRow[] = [
+    {
+        id: '1',
+        mode: 'CASUAL',
+        type: 'PROGRAMMING',
+        timestamp: '2H 30M AGO',
+        result: 'WIN',
+        details: {
+            results: '3-2',
+            matchLength: '05:00',
+            questions: [
+                {
+                    label: 'QUESTION 1',
+                    speed: '00:22',
+                    accuracy: '90%',
+                    correctness: true,
+                },
+            ],
+            date: 'JULY 26, 2026',
+            time: '10:25 - 10:28',
+        },
+    },
+];
