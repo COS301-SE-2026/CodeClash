c695be9cb5141092daefee37e0550b11253e55d8
nsmzoneli <nsmzoneli@gmail.com>
2026-07-26 20:51:19 +0200

    added the persistent match entity

--- backend/src/entities/db-entities/match.entities.ts
+++ backend/src/entities/db-entities/match.entities.ts
@@ -1,6 +1,5 @@
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Users } from './user.entities';
-import { Match } from "../ecs-entities";
 
 @Entity()
 export class MatchProblems{
@@ -8,6 +7,36 @@ export class MatchProblems{
     match_problems_id!: string
 
     
+}
+
+@Entity()
+export class Match {
+    @PrimaryGeneratedColumn('uuid')
+    match!: string;
+
+    @OneToOne(() => Users)
+    @JoinColumn({ name: 'player1_id'})
+    player1!: Users;
+
+    @OneToOne(() => Users)
+    @JoinColumn({ name: 'player2_id'})
+    player2!: Users;
+
+    @OneToOne(() => MatchProblems, { nullable: true})
+    @JoinColumn({ name: 'match_problems' })
+    match_problems!: MatchProblems | null;
+
+    @Column({ type: 'varchar', length: 10})
+    mode!: 'ranked' | 'casual';
+
+    @CreateDateColumn()
+    queue_start!: Date;
+
+    @Column({ type: 'timestamp', nullable: true })
+    match_start!: Date | null;
+
+    @Column({ default: 'waiting'})
+    status!: 'waiting' | 'starting' | 'in_progress' | 'completed' | 'abandoned';
 }
 
 @Entity()
