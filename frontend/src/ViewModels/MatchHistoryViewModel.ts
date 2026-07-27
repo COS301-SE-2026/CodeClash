70f87af0180d542a2062a83abee909fb34c76d4b
TaskeenAbdoola <u22698681@tuks.co.za>
2026-07-27 13:38:03 +0200

    updated with functions and return

--- frontend/src/ViewModels/MatchHistoryViewModel.ts
+++ frontend/src/ViewModels/MatchHistoryViewModel.ts
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback} from "react";
 import { mockedMatch } from "../Models/MatchHistoryModel";
 import type { MatchRow } from "../Models/MatchHistoryModel";
 
@@ -6,6 +6,30 @@ interface MatchHistoryViewModel {
     matches: MatchRow[];
     selected: MatchRow | null;
     isDetails: boolean;
-    handleRowCLick: (match: MatchRow) => void;
+    handleRowClick: (match: MatchRow) => void;
     handleCloseDetails: () => void;
+}
+
+export function MatchHistoryViewModelFunction(): MatchHistoryViewModel {
+    const [selected, setSelected] = useState<MatchRow | null>(null);
+    const [isDetails, setIsDetails] = useState(false);
+    const matches = mockedMatch; //MOCKED TO TEST
+
+    const handleRowClick = useCallback((match: MatchRow) => {
+        setSelected(match);
+        setIsDetails(true);
+    }, []);
+
+    const handleCloseDetails = useCallback(() => {
+        setIsDetails(false);
+        setTimeout(() => setSelected(null), 300);
+    }, []);
+
+    return {
+        matches,
+        selected,
+        isDetails,
+        handleRowClick,
+        handleCloseDetails,
+    };
 }
