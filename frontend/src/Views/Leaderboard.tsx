315e7636c159e32000a8023a5d43d0a5446b4c4d
Ntuuu <apathylely@gmail.com>
2026-07-27 12:05:02 +0200

    Testing of husky stuff

--- frontend/src/Views/Leaderboard.tsx
+++ frontend/src/Views/Leaderboard.tsx
@@ -1,10 +1,12 @@
+import background from '../assets/Background/leaderboard_background.png';
 import profile from '../assets/Icons/Profile.png';
-import shadow from '../assets/shadow.png';
 import winner from '../assets/Icons/winner.png';
 import planet from '../assets/Planets/Earth.png';
-import background from '../assets/Background/leaderboard_background.png';
+import shadow from '../assets/shadow.png';
+
 import '../styles/global.css';
 import {Link} from 'react-router-dom';
+
 import light from '../assets/light.png';
 
 
@@ -23,14 +25,14 @@ const Leaderboard = (
 //     eigthUser = LeaderboardUserData,
 //     ninthUser = LeaderboardUserData,
 //     tenthUser = LeaderboardUserData
-// 
+//
 //}
 ) => {
-    
+
     return(
     // <div className="w-full min-h-screen bg-[#C0395A]">
     // <div className="w-2/3 bg-[#C0395A]">
-    
+
     // <h1 className="mt-13 ml-80 text-[64px] text-[#FFDBE4] font-[var(--font)] font-heading-weight drop-shadow-[4px_4px_0px_rgba(0,0,0,0.25)]">Leader Board</h1>
     // <div className="grid grid-flow-col grid-cols-3 gap-6">
     //     <div className="ml-5 w-[239px] h-[304px] rounded-[20px] bg-[#FAC8D0] border-4 border-[#F4A0B2] drop-shadow-[10px_10px_6px_rgba(0,0,0,0.25)]"></div>
@@ -41,7 +43,7 @@ const Leaderboard = (
     <div style={{backgroundImage: `url(${shadow})`}} className="w-full min-h-screen bg-[#C0395A] bg-no-repeat object-cover bg-size-[auto_150px] bg-[19%_60%]">
         <Link to="/dashboard" className="text-[#530A24] font-[var(--font)] text-[32px] font-semibold ml-5">← Back</Link>
         <h1 className="text-[60px] text-[#FFDBE4] text-center -mt-1 mr-230 font-[var(--font)] font-semibold drop-shadow-[4px_4px_0px_rgba(0,0,0,0.25)]">Leader Board</h1>
-        
+
         <div className="flex justify-between items-start gap-10">
             <div className="relative w-2/3 flex flex-col gap-6">
             <div className="grid grid-cols-3 gap-9 items-end">
@@ -122,3 +124,5 @@ const Leaderboard = (
 }
 
 export default Leaderboard;
+
+// testing husky stuff
