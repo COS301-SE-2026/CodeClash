287241a5a9c580871ab1e80ad11ed6b92c67bc4c
morganthegirlboss <u24910882@tuks.co.za>
2026-07-27 08:24:50 +0200

    added text for scoring system in new game guide

--- frontend/src/Views/GameGuide.tsx
+++ frontend/src/Views/GameGuide.tsx
@@ -15,7 +15,7 @@ const GameGuide = () => {
 
                     {/* the text below is pasted from a wireframe, no AI was utilised in the generation of any of this code */}
                     
-                    <div className="w-[120%] font-font font-bold text-[#FFFFFF] text-[220%] mx-auto">
+                    <div className="w-[120%] font-font font-bold text-[#FFFFFF] text-[200%] mx-auto">
                         This game guide serves to provide everything needed to get started with CodeClash. Learn how matches work, understand the scoring system, explore the game rules, and discover tips to improve your performance. Whether you’re a first time player or a seasoned competitor, this guide will help you navigate the platform, make the most of every challenge, and compete with confidence.
                     </div>
                     <div className="ml-[6%] flex flex-col grid grid-cols-2">
@@ -28,6 +28,12 @@ const GameGuide = () => {
                 </div>
 
                 <div className="bg-[var(--color-pink-700)] w-[22%] h-[6%] text-[#FFFFFF] text-[260%] font-bold font-font text-center mt-[10%] ml-[2%]">SCORING SYSTEM</div>
+                
+                {/* the text below was copied and pasted from a wireframe, no AI was used to generate any of this code */}
+                
+                <div className="w-[95%] text-[#FFFFFF] text-[190%] font-bold font-font ml-[2%] mt-[1%] mx-auto">
+                    Every answer submitted is automatically compared against the correct solution using the game's validation system. Correct answers earn points, while incorrect answers receive no score. If multiple players answer correctly, the player who submits the correct solution in the shortest amount of time receives a higher score, rewarding quick thinking under pressure. This balanced scoring system ensures that success is determined not only by being fast, but also by writing accurate and correct solutions. At the end of each match, players can review their results, compare their performance, and identify areas for improvement before taking on their next challenge
+                </div>
             </div>
 
 
