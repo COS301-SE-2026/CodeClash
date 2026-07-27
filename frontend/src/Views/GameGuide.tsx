8d5dcb4871201c5c743ee9c1b9b50606556d2f9f
morganthegirlboss <u24910882@tuks.co.za>
2026-07-27 07:28:03 +0200

    added top text to page

--- frontend/src/Views/GameGuide.tsx
+++ frontend/src/Views/GameGuide.tsx
@@ -11,8 +11,13 @@ const GameGuide = () => {
 
             <div className="bg-[#000000]/20 mx-auto mt-[1%] w-[95%] h-[95%] rounded-[20px] backdrop-blur-sm border-10 border-[#530A24]">
                 <h1 className="font-font font-bold text-secondary text-[230%] mx-auto text-center mt-[3%]">CODECLASH - Game Guide</h1>
-                <div className="flex flex-col grid grid-cols-2 grid rows-2">
-    
+                <div className="flex flex-col grid grid-cols-2 grid rows-2 w-[90%] h-[30%] border">
+
+                    {/* the text below is pasted from a wireframe, no AI was utilised in the generation of any of this code */}
+                    
+                    <p className="font-font font-bold text-[#FFFFFF] text-[100%] mx-auto">
+                        This game guide serves to provide everything needed to get started with CodeClash. Learn how matches work, understand the scoring system, explore the game rules, and discover tips to improve your performance. Whether you’re a first time player or a seasoned competitor, this guide will help you navigate the platform, make the most of every challenge, and compete with confidence.
+                    </p>
                 </div>
             </div>
 
