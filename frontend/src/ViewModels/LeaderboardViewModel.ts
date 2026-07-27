f3a74297d724d11d2eff6011eac4b8aa36ac1135
Ntuuu <apathylely@gmail.com>
2026-07-27 11:55:03 +0200

    Added: addition of leaderboardviewmodel, so far is just a copy of Morgans version as it pulling wasnt working properly, will work on fixing the gitblame and return rightfully what belonged to her, to her later

---
+++ frontend/src/ViewModels/LeaderboardViewModel.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect, useCallback } from 'react'
+import type { LeaderboardUserProps } from 'src/Models/LeaderboardModel';
+import { fetchLeaderboardUsers } from 'src/Models/LeaderboardModel';
+
+
+export function LeaderboardViewModel(league : string){
+    const [userData, setUserData] = useState<LeaderboardUserProps[]>([]);
+    const [isLoadingData, setIsLoadingData] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    const loadLeaderboard = useCallback(async () => { //useCallback is used so that returned data is cached until any values of the returned data is changed
+        setIsLoadingData(true);
+        setError(null);
+        try{
+            const data = await fetchLeaderboardUsers(league);
+            setUserData(data);
+        }
+        catch(err){
+            setError('Could not load User Data');
+        }
+        finally{
+            setIsLoadingData(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        loadLeaderboard();
+    }, [loadLeaderboard]);
+
+    const topTen = userData.slice(0,10);
+
+   return {userData, topTen, isLoadingData, error, refresh: loadLeaderboard}
+
+
+}
