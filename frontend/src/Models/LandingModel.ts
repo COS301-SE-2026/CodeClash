613a198cd6a013aec31af90675dbc36489102a67
TaskeenAbdoola <u22698681@tuks.co.za>
2026-07-28 22:38:22 +0200

    Starting landing model - stats section

---
+++ frontend/src/Models/LandingModel.ts
@@ -0,0 +1,23 @@
+export interface Stat {
+    value: string;
+    label: string;
+}
+
+export const stats: Stat[] = [
+    {
+        value: '1v1',
+        label: 'Real-time battles',
+    },
+    {
+        value: '2',
+        label: 'Game modes',
+    },
+    {
+        value: 'ELO',
+        label: 'Ranked system',
+    },
+    {
+        value: 'Infinity',
+        label: 'Problems to solve',
+    }
+]
