5070292ec1980781c2c2d4e2d2d7b618d2d3e03a
TaskeenAbdoola <u22698681@tuks.co.za>
2026-07-28 23:37:09 +0200

    the view model function and its returns

--- frontend/src/ViewModels/LandingViewModel.ts
+++ frontend/src/ViewModels/LandingViewModel.ts
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import {stats, steps, features, players, audience} from '../Models/LandingModel';
 import type { Stat, Step, Feature, PlayerProgress,} from '../Models/LandingModel';
 
-export interface LandingVIewModelProps {
+export interface LandingViewModelProps {
     scrollY: number;
     stats: Stat[];
     steps: Step[];
@@ -11,3 +11,31 @@ export interface LandingVIewModelProps {
     audience: string[];
     players: PlayerProgress[];
 }
+
+export const LandingViewModelFunction = (): LandingViewModelProps => {
+    const [scrollY, setScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        }
+
+        window.addEventListener("scroll", handleScroll, {
+            passive: true,
+        })
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }, []);
+
+    return {
+        scrollY,
+
+        stats,
+        steps,
+        features,
+        audience,
+        players,
+    };
+};
