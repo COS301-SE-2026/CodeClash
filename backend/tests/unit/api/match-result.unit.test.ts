0ae5e373c7077ef4fb5ad21f648434c267f22a6e
nsmzoneli <nsmzoneli@gmail.com>
2026-07-29 00:04:40 +0200

    cleaned up the imports in my testing files, they pass

--- backend/tests/unit/api/match-result.unit.test.ts
+++ backend/tests/unit/api/match-result.unit.test.ts
@@ -1,7 +1,4 @@
 import { vi, Mock, describe, beforeEach, it, expect} from 'vitest';
-import { MatchResultService } from '../../../src/application/usecases/services/match-result.service';
-import { IEloRepository } from '../../../src/application/interfaces/repositories/IEloRepository';
-import { IMatchResultRepository } from '../../../src/application/interfaces/repositories/IMatchResultRepository';
 import { getMatchResults } from '../../../src/interface-adapters/controllers/match-results.controllers';
 import { Response, Request } from 'express';
 
