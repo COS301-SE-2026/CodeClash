b914ea6a00033d62743b3b78bb9587f281aa08ae
TaskeenAbdoola <u22698681@tuks.co.za>
2026-07-28 22:50:40 +0200

    Steps for app

--- frontend/src/Models/LandingModel.ts
+++ frontend/src/Models/LandingModel.ts
@@ -3,6 +3,13 @@ export interface Stat {
     label: string;
 }
 
+export interface Step {
+    step: string;
+    icon: "rocket" | "swords" | "trophy";
+    title: string;
+    desc: string;
+}
+
 export const stats: Stat[] = [
     {
         value: '1v1',
@@ -19,5 +26,26 @@ export const stats: Stat[] = [
     {
         value: 'Infinity',
         label: 'Problems to solve',
+    }
+]
+
+export const steps:Step[] = [
+    {
+        step: '01',
+        icon: 'rocket',
+        title: 'Choose your battle',
+        desc: 'Pick between modes Casual or Ranked.',
+    },
+    {
+        step: '02',
+        icon: 'swords',
+        title: 'Compete Live',
+        desc: 'Solve problems faster and more accurately than your opponent. Watch their progress in real-time. Submit answers before they do.',
+    },
+    {
+        step: '02',
+        icon: 'trophy',
+        title: 'CLimb the Ranks',
+        desc: 'Your ELO updates after every match. Earn nadges, trcak your history, and rise through the leaderboard.',
     }
 ]
