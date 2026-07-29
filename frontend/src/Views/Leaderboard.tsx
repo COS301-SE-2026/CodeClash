9ef253dd8cfa61878ea63abb33149719c8702805
morganthegirlboss <u24910882@tuks.co.za>
2026-07-29 12:43:38 +0200

    adding usercircle lucid react component instead and fixing leaderboard card flex boxes

--- frontend/src/Views/Leaderboard.tsx
+++ frontend/src/Views/Leaderboard.tsx
@@ -1,4 +1,5 @@
 import profile from '../assets/Icons/profile_black.png';
+import { UserCircle } from 'lucide-react';
 import winner from '../assets/Icons/winner.png';
 import planet from '../assets/Planets/Earth.png';
 import background from '../assets/Background/leaderboard_background.png';
@@ -12,20 +13,6 @@ import LeaderboardCardLeft, {LeaderboardCardRight, EloText} from '@/components/u
 
 // const Leaderboard: React.FC<LeaderboardProps> = (
 const Leaderboard = (league : string) => {
-    const { isLoadingData, error, refresh} = LeaderboardViewModel(league);
-
-    // if(isLoadingData){
-    //     return <div className="text-center mx-auto font-[var(--font)] text-[200%] font-semibold text-button-primary">Fetching Leaderboard Data</div>
-    // }
-
-    // if(error){
-    //     return(
-    //         <div className="text-center mx-auto font-[var(--font)] text-[200%] font-semibold text-button-primary">
-    //             {error}
-    //             <button onClick={refresh} className="text-secondary bg-button-primary font-[var(--font)] font-semibold" type="button">Refresh</button>
-    //         </div>
-    //     );
-    // }
     
     return(
     <div style={{backgroundImage: `url(${bg})`}} className="w-full min-h-screen bg-cover bg-no-repeat object-cover">
@@ -37,17 +24,20 @@ const Leaderboard = (league : string) => {
             <div className="flex grid grid-cols-3 gap-4 h-full w-full">
                 <LeaderboardCardLeft className="mt-[7%] ml-[58%]"></LeaderboardCardLeft>
                 <div style={{backgroundImage : `url(${winner})`}} className="w-[83%] h-[95%] bg-no-repeat object-cover bg-size-[auto_130%] bg-[left_49%_top_55%] justify-center mt-[10%] ml-[9.5%]">
-                    <div style={{backgroundImage : `url(${profile})`}} className="w-[120px] h-[120px] bg-no-repeat rounded-full object-cover bg-size-[auto_200%] bg-[right_49.5%_top_49%] mx-auto"></div>
+                    <UserCircle className="w-[120px] h-[120px] bg-no-repeat rounded-full object-cover bg-size-[auto_200%] bg-[right_49.5%_top_49%] mx-auto"></UserCircle>
                     <div className="text-[360%] text-[#B39A48] text-center font-[var(--font)] font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">1</div>
                     <EloText className="text-[165%]">Username</EloText>
                     <EloText className="text-[165%]">ELO</EloText>
                 </div>
                 <LeaderboardCardRight className="mt-[7%]"></LeaderboardCardRight>
                 <div className="w-[45%] h-[85%] rounded-[20px] border-4 border-button-primary bg-secondary ml-[58%] -mt-[15%]">
-                    <div style={{backgroundImage: `url(${profile})`}} className="w-[59px] h-[59px] bg-no-repeat rounded-full object-cover bg-size-[auto_200%] bg-[right_49%_top_50%] mt-[2%]">
-                        <EloText className="text-[200%] ml-[115%] -mt-[10%]">4</EloText>
-                        <div className="text-[80%] text-button-primary text-center font-[var(--font)] font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] ml-[222%] -mt-[90%]">Username</div>
-                        <div className="text-[80%] text-button-primary text-center font-[var(--font)] font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] ml-[260%] -mt-[5%]">ELO</div>
+                    <div className="flex flex-col grid grid-cols-3">
+                    <UserCircle className="w-[59px] h-[59px] bg-no-repeat rounded-full object-cover bg-size-[auto_200%] bg-[right_49%_top_50%]"></UserCircle>
+                        <EloText className="text-[200%] -ml-[70%] -mt-[5%]">4</EloText>
+                        <div className="flex flex-row grid grid-rows-2">
+                        <div className="text-[80%] text-button-primary text-center font-[var(--font)] font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] -ml-[40%] mt-[2%]">Username</div>
+                        <div className="text-[80%] text-button-primary text-center font-[var(--font)] font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] -mt-[5%]">ELO</div>
+                        </div>
                     </div>
                 </div>
                 <div className="w-[45%] h-[85%] rounded-[20px] border-4 border-button-primary bg-secondary ml-[103%] -mt-[15%]">
