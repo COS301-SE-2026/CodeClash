e3982ff4f6b3ff88a25c01d0e47368270e01ac8c
Ntuuu <apathylely@gmail.com>
2026-07-29 22:06:41 +0200

    Changed: api.routes.ts in order to account for modified inputs and modified routes based on changing methods

--- backend/src/frameworks-drivers/routes/api.routes.ts
+++ backend/src/frameworks-drivers/routes/api.routes.ts
@@ -1,24 +1,24 @@
 import { Router } from 'express';
-
+import { LeaderboardSystem } from 'src/application/usecases/systems/leaderboard.system';
+import { EloRatings } from 'src/entities/db-entities/elo.entities';
+import { Users } from 'src/entities/db-entities/user.entities';
 import {
   getUserElo,
-  getEloHistory,
-  updateEloAfterMatch,
-  getLeaderboard,
-  setUserElo
 } from 'src/interface-adapters/controllers/elo.controllers';
+import { getLeaderboardController } from 'src/interface-adapters/controllers/leaderboard.controller';
 // import { getMatches, getMatchById, createMatch, updateMatchStatus, getMatchLog } from 'src/interface-adapters/controllers/matches.controllers';
 import { getUserStat } from 'src/interface-adapters/controllers/user.controllers';
-import { UserRepository } from 'src/interface-adapters/repositories/user.repository';
-import { AppDataSource } from '../config/data-source';
-import { Users } from 'src/entities/db-entities/user.entities';
 import { EloRepository } from 'src/interface-adapters/repositories/elo.repository';
-import { EloRatings } from 'src/entities/db-entities/elo.entities';
+import { UserRepository } from 'src/interface-adapters/repositories/user.repository';
+
+import { AppDataSource } from '../config/data-source';
+
 
 const router = Router();
 
 const user_repo = new UserRepository(AppDataSource.getRepository(Users))
 const elo_repo = new EloRepository(AppDataSource.getRepository(EloRatings))
+const leaderboard_system = new LeaderboardSystem(elo_repo);
 
 // // Match routes
 // router.get('/matches', getMatches);
@@ -28,7 +28,7 @@ const elo_repo = new EloRepository(AppDataSource.getRepository(EloRatings))
 // router.get('/matches/:match_id/log', getMatchLog);
 
 //elo routes
-router.get('/leaderboard', getLeaderboard);
+router.get('/leaderboard', getLeaderboardController(leaderboard_system));
 router.get('/elo-get', getUserElo(elo_repo));
 router.post('/elo-history', getEloHistory);
 router.post('/update', updateEloAfterMatch);
