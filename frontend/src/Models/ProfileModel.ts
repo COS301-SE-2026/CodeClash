bbed3ebe098329b10e834a93fc7a62269e0aad74
morganthegirlboss <u24910882@tuks.co.za>
2026-07-29 20:13:29 +0200

    changing profile props in model as requested

--- frontend/src/Models/ProfileModel.ts
+++ frontend/src/Models/ProfileModel.ts
@@ -1,11 +1,11 @@
 
 
 export interface ProfileProps {
-    username?: string;
-    email?: string;
-    player_level?: string;
-    current_streak?: number;
-    prev_page?: string;
+    avatarUrl: string;
+    username: string;
+    rank: number;
+    elo: number;
+    league: string;
 }
 
 
