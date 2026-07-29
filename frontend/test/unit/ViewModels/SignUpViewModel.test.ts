c33655e03a64d7a409d2719eb7b3e301090c5ad0
morganthegirlboss <u24910882@tuks.co.za>
2026-07-29 10:18:12 +0200

    working on test for sign up view model, adding mock for auth

--- frontend/test/unit/ViewModels/SignUpViewModel.test.ts
+++ frontend/test/unit/ViewModels/SignUpViewModel.test.ts
@@ -2,9 +2,12 @@ import { renderHook, act } from "@testing-library/react";
 import { describe, it, expect, vi } from "vitest";
 import {SignUpViewModelFunction} from "../../../src/ViewModels/SignUpViewModel"
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../src/context/Auth/hooks/useAuth";
 
 
 const mock_nav = vi.fn();
 vi.mock('react-router', () => ({  //apparently there is a security issue with react-router-dom so i am making this react-router
     useNavigate: () => mock_nav
 }))
+
+vi.mock('../../../src/context/Auth/hooks/useAuth');
