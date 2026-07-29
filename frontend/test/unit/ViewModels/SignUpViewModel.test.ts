b2faa9cca0a29a1cdaf0818a70d4f3020746d3bc
morganthegirlboss <u24910882@tuks.co.za>
2026-07-29 09:58:00 +0200

    starting on test for sign up view model

---
+++ frontend/test/unit/ViewModels/SignUpViewModel.test.ts
@@ -0,0 +1,3 @@
+import { renderHook, act } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import {SignUpViewModelFunction} from "../../../src/ViewModels/SignUpViewModel"
