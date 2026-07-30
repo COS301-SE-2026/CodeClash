9f78ea851424cf855535733cf2f5e374f363d21e
morganthegirlboss <u24910882@tuks.co.za>
2026-07-30 23:18:30 +0200

    mocking popup functions, different outcomes

--- frontend/test/unit/ViewModels/PopupViewModel.test.ts
+++ frontend/test/unit/ViewModels/PopupViewModel.test.ts
@@ -1,9 +1,11 @@
-import {beforeEach, describe, vi} from "vitest"
+import {beforeEach, describe, it, vi, expect} from "vitest"
 import { renderHook } from "@testing-library/react"
 import { act } from "react"
 import { useSocket } from "../../../src/context/Socket/hooks/useSocket"
 import { useUser } from "../../../src/context/User/hooks/useUser"
 import { useSelectTopic } from "../../../src/ViewModels/PopUpViewModel"
+import { joinMatchQueue } from "../../../src/context/Socket/hooks/useMatchmakingSocket"
+import MatchmakingUserDTO from "../../../src/dtos/matchmaking.dto"
 
 
 const mock_nav = vi.fn();
@@ -23,8 +25,28 @@ const mockSocket = {id : 'mock-socket'} as unknown as ReturnType<typeof useSocke
 
 beforeEach(() => {
     vi.clearAllMocks();
-    vi.mocked(useSocket).mockReturnValue({socket : mockSocket, isConnected: true, matched: null}) as unknown as ReturnType<typeof useSocket>);
+    vi.mocked(useSocket).mockReturnValue({socket : mockSocket, isConnected: true, matched: {game_mode: 'maths', pair_id: '01234VWXYZ'}});
     vi.mocked(useUser).mockReturnValue({ elo: 600} as ReturnType<typeof useUser>);
 
 });
+
+describe('useSelectTopic', () => {
+    it('throws an error if there is no active socket', async () => {
+        vi.mocked(useSocket).mockReturnValue({socket : null, isConnected : false, matched : null}); //creates a socket that is not active
+        const { result } = renderHook(() => useSelectTopic());
+
+        await expect(act( () => result.current('maths'))).rejects.toThrow('500 Internal Server Error');
+        expect(joinMatchQueue).not.toHaveBeenCalled();
+        expect(mock_nav).not.toHaveBeenCalled();
+    });
+
+    it('creates new dto with selected topic, then joins match queue', async () => {
+
+        const { result } = renderHook(() => useSelectTopic());
+
+        await act( () => result.current('maths'));
+        expect(MatchmakingUserDTO)
+
+    })
+})
 
