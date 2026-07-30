f3cb1ebc07b6e1a969bab7622cbd50e10cad00cc
nsmzoneli <nsmzoneli@gmail.com>
2026-07-30 23:20:21 +0200

    changed ports to what they should be

--- backend/src/frameworks-drivers/app.ts
+++ backend/src/frameworks-drivers/app.ts
@@ -13,7 +13,7 @@ export function createApp(user_repo: IUserRepository) {
     res.json({ status: 'ok' });
   });
 
-  app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:4444' }));
+  app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' }));
   app.use(express.json());
   app.use(requireAuth(user_repo))
 
