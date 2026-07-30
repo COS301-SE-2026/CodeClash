d6eca9906369097c69fe769a50a41f71a7beb583
Ntuuu <apathylely@gmail.com>
2026-07-30 21:45:08 +0200

    Changed: moved leaderboard system to service, didnt change the name from system to service though cuz i dont have the time to update all that

--- backend/src/application/usecases/services/leaderboard.service.ts
+++ backend/src/application/usecases/services/leaderboard.service.ts
@@ -5,10 +5,20 @@ export class LeaderboardSystem {
     constructor(private eloRepository: IEloRepository) {}
 
     async execute(limit: number, page: number): Promise<PaginatedLeaderboardResponse> {
+      // const offset = (page - 1) * limit;
+      // const { data, total } = await this.eloRepository.getLeaderboard(limit, offset);
       const offset = (page - 1) * limit;
       const { data, total } = await this.eloRepository.getLeaderboard(limit, offset);
       return {
-        data: data.map((entry, index) => ({ ...entry, rank: offset + index + 1 })),
+        // data: data.map((entry, index) => ({ ...entry, rank: offset + index + 1 })),
+        // total,
+        // page,
+        // pageSize: limit,\
+        data: data.map((entry, index) => ({
+          ...entry,
+          rank: offset + index + 1,
+          
+        })),
         total,
         page,
         pageSize: limit,
