3073e593028d42c762ac63893834916cd18ed16e
morganthegirlboss <u24910882@tuks.co.za>
2026-07-30 21:45:06 +0200

    continuing popup view model testing, creating a mock socket

--- frontend/test/unit/ViewModels/PopupViewModel.test.ts
+++ frontend/test/unit/ViewModels/PopupViewModel.test.ts
@@ -1,7 +1,8 @@
-import {describe, vi} from "vitest"
+import {beforeEach, describe, vi} from "vitest"
 import { renderHook } from "@testing-library/react"
 import { act } from "react"
 import { useSocket } from "../../../src/context/Socket/hooks/useSocket"
+import { useUser } from "../../../src/context/User/hooks/useUser"
 
 
 const mock_nav = vi.fn();
@@ -12,3 +13,15 @@ vi.mock('react-router', () => ({
 vi.mock('../../../src/context/Socket/hooks/useSocket', () => ({
     useSocket: vi.fn()
 })) 
+
+vi.mock('../../../src/context/User/hooks/useUser', () => ({
+    useUser: vi.fn()
+}))
+
+const mockSocket = 
+
+beforeEach(() => {
+    vi.clearAllMocks();
+
+})
+
