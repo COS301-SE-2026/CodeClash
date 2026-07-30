57a326ccefa09af0d6093ff97fe7365c7f5c545a
morganthegirlboss <u24910882@tuks.co.za>
2026-07-30 21:16:13 +0200

    continuing popup view model testing

--- frontend/test/unit/ViewModels/PopupViewModel.test.ts
+++ frontend/test/unit/ViewModels/PopupViewModel.test.ts
@@ -1,3 +1,14 @@
 import {describe, vi} from "vitest"
 import { renderHook } from "@testing-library/react"
 import { act } from "react"
+import { useSocket } from "../../../src/context/Socket/hooks/useSocket"
+
+
+const mock_nav = vi.fn();
+vi.mock('react-router', () => ({
+    useNavigate: () => mock_nav,
+}))
+
+vi.mock('../../../src/context/Socket/hooks/useSocket', () => ({
+    useSocket: vi.fn()
+})) 
