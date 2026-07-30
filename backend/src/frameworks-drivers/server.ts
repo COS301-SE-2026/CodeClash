5f46851de1d3a57352bcee84002a41f79181e155
Ntuuu <apathylely@gmail.com>
2026-07-31 01:19:46 +0200

    Debug logs

--- backend/src/frameworks-drivers/server.ts
+++ backend/src/frameworks-drivers/server.ts
@@ -7,19 +7,19 @@ import { IQuestionRepository } from 'src/application/interfaces/IQuestionReposit
 import { IUserRepository } from 'src/application/interfaces/IUserRepository';
 import { initDB } from 'src/application/usecases/init-db';
 import { EloRatings } from 'src/entities/db-entities/elo.entities';
+import { Questions } from 'src/entities/db-entities/questions.entities';
 import { EloRepository } from 'src/interface-adapters/repositories/elo.repository';
+import { QuestionRepository } from 'src/interface-adapters/repositories/question.repository';
 import { UserRepository } from 'src/interface-adapters/repositories/user.repository';
 import { sendGameQuestions, joinMatchQueue, leaveMatchQueue, matchAccepted, matchDeclined } from 'src/interface-adapters/socket-handlers/matchmaking.handler';
 
 import { Users } from "../entities/db-entities/user.entities"
 import { validateToken } from '../interface-adapters/auth/auth.service';
-import app from './app';
 
+import app from './app';
 import { AppDataSource } from "./config/data-source"
 
 
-import { QuestionRepository } from 'src/interface-adapters/repositories/question.repository';
-import { Questions } from 'src/entities/db-entities/questions.entities';
 
 dotnev.config()
 
@@ -37,7 +37,7 @@ const io = new Server(httpServer, {
 );
 
 
-// auth middleware 
+// auth middleware
 io.use(async (socket, next) => {
     const token = socket.handshake.auth.token;
 
@@ -67,9 +67,13 @@ AppDataSource.initialize()
 
         // attach socket handlers
         io.on("connection", (socket) => {
-
+          console.log("socket connected", socket.id);
+          console.log("user_id:", socket.data.user_id)
             // SOCKET HANDLERS MUST MOOVE TO interface-adapter/
-            socket.on('join_match_queue', async (data) => await joinMatchQueue(io, socket, data));
+          socket.on('join_match_queue', async (data) => {
+            console.log("join match queue", socket.id, data);
+            await joinMatchQueue(io, socket, data)
+          });
 
             socket.on('leave_match_queue', async () => await leaveMatchQueue(io, socket));
 
