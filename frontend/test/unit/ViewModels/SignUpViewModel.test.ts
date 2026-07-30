0c1ef1a85dcfdb4dc9db999f382bf441a3258baa
morganthegirlboss <u24910882@tuks.co.za>
2026-07-30 23:43:48 +0200

    working on sign up view model testing, testing for missing or invalid formfields

--- frontend/test/unit/ViewModels/SignUpViewModel.test.ts
+++ frontend/test/unit/ViewModels/SignUpViewModel.test.ts
@@ -1,6 +1,6 @@
 import { renderHook, act } from "@testing-library/react";
 import { describe, it, expect, vi } from "vitest";
-import {SignUpViewModelFunction} from "../../../src/ViewModels/SignUpViewModel"
+import {SignUpViewModelFunction, validateSignUpForm} from "../../../src/ViewModels/SignUpViewModel"
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../src/context/Auth/hooks/useAuth";
 import { SignUpForm } from "../../../src/Models/SignUpModel";
@@ -22,4 +22,12 @@ const validForm: SignUpForm = {
     password: 'Password1!',
     acceptedTerms: true
 };
+
+
+describe('validateSignUpForm', () => {
+    it('returns an error message for any missing or invalid field', () => {
+        expect(validateSignUpForm({...validForm, username: ''})).toBe('Username is required'); // ... is a rest operator meaning we take all the values of validform and clone them into a new object of the same type EXCEPT for the variable that is paired with the object that has the rest operator in front of it, in this case, that is username
+        expect(validateSignUpForm({...validForm, password: 'bad'})).toBe('Password must be atleast 8 characters');
+    })
+})
 
