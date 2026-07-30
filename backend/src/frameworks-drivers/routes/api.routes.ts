41e416bc29e41a0e428ac9300105479a217a905c
Ntuuu <apathylely@gmail.com>
2026-07-30 13:21:52 +0200

    Removed: old api routing to avoid conflicts

--- backend/src/frameworks-drivers/routes/api.routes.ts
+++ backend/src/frameworks-drivers/routes/api.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { LeaderboardSystem } from 'src/application/usecases/systems/leaderboard.system';
+// import { LeaderboardSystem } from 'src/application/usecases/systems/leaderboard.system';
 import { EloRatings } from 'src/entities/db-entities/elo.entities';
 import { Users } from 'src/entities/db-entities/user.entities';
 import {
   getUserElo,
 } from 'src/interface-adapters/controllers/elo.controllers';
-import { getLeaderboardController } from 'src/interface-adapters/controllers/leaderboard.controller';
+// import { getLeaderboardController } from 'src/interface-adapters/controllers/leaderboard.controller';
 // import { getMatches, getMatchById, createMatch, updateMatchStatus, getMatchLog } from 'src/interface-adapters/controllers/matches.controllers';
 import { getUserStat } from 'src/interface-adapters/controllers/user.controllers';
 import { EloRepository } from 'src/interface-adapters/repositories/elo.repository';
@@ -18,7 +18,7 @@ const router = Router();
 
 const user_repo = new UserRepository(AppDataSource.getRepository(Users))
 const elo_repo = new EloRepository(AppDataSource.getRepository(EloRatings))
-const leaderboard_system = new LeaderboardSystem(elo_repo);
+// const leaderboard_system = new LeaderboardSystem(elo_repo);
 
 // // Match routes
 // router.get('/matches', getMatches);
@@ -28,7 +28,7 @@ const leaderboard_system = new LeaderboardSystem(elo_repo);
 // router.get('/matches/:match_id/log', getMatchLog);
 
 //elo routes
-router.get('/leaderboard', getLeaderboardController(leaderboard_system));
+// router.get('/leaderboard', getLeaderboardController(leaderboard_system));
 router.get('/elo-get', getUserElo(elo_repo));
 // router.post('/elo-history', getEloHistory);
 // router.post('/update', updateEloAfterMatch);
