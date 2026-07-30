6523dc01ba1385a155a102ada522927cefab3829
Ntuuu <apathylely@gmail.com>
2026-07-30 13:21:31 +0200

    Changed: moved leaderboard routing to be put as a public api and to be run before auth

--- backend/src/frameworks-drivers/app.ts
+++ backend/src/frameworks-drivers/app.ts
@@ -1,10 +1,15 @@
 import cors from 'cors'
 import express, { Request, Response } from 'express'
-import routes from './routes/api.routes';
-import { UserRepository } from 'src/interface-adapters/repositories/user.repository';
-import { AppDataSource } from './config/data-source';
+import { LeaderboardSystem } from 'src/application/usecases/systems/leaderboard.system';
+import { EloRatings } from 'src/entities/db-entities/elo.entities';
 import { Users } from 'src/entities/db-entities/user.entities';
 import { requireAuth } from 'src/interface-adapters/auth/auth.service';
+import { getLeaderboardController } from 'src/interface-adapters/controllers/leaderboard.controller';
+import { EloRepository } from 'src/interface-adapters/repositories/elo.repository';
+import { UserRepository } from 'src/interface-adapters/repositories/user.repository';
+
+import { AppDataSource } from './config/data-source';
+import routes from './routes/api.routes';
 
 const app = express();
 app.disable('x-powered-by');
@@ -14,6 +19,10 @@ app.get('/health', (req: Request, res: Response) => {
 
 app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' }));
 app.use(express.json());
+
+const elo_repo = new EloRepository(AppDataSource.getRepository(EloRatings))
+const leaderboard_system = new LeaderboardSystem(elo_repo);
+app.get('/api/elo/leaderboard', getLeaderboardController(leaderboard_system));
 
 const user_repo = new UserRepository(AppDataSource.getRepository(Users))
 app.use(requireAuth(user_repo))
