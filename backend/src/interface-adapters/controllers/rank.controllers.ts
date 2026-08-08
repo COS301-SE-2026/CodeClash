dbe21d961b2981a54f2bd4fd96446ba62b2590d4
morganthegirlboss <u24910882@tuks.co.za>
2026-08-08 18:18:38 +0200

    working on logic for rank controller, i may have to come back to fix this as i am confused about which id is retrieved from user.id

--- backend/src/interface-adapters/controllers/rank.controllers.ts
+++ backend/src/interface-adapters/controllers/rank.controllers.ts
@@ -3,6 +3,14 @@ import { LeaderboardSystem } from "src/application/usecases/services/leaderboard
 
 
 export const getUserRank = (service: LeaderboardSystem) => {
-    return async(req: Request, res: Response){
-        
-    }
+    return async(req: Request, res: Response) => {
+        try{
+            const userId = req.user?.id //after checking auth.service.ts, this id value is the same as user_id
+            
+            if
+        }
+        catch(error){
+            res.status(500).json({message: `Error getting user rank. Error: ${error}`})
+        }
+    };
+}
