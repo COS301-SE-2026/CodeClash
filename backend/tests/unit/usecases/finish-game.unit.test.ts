2360fef1f9d9c898a8629f45bdd3ede6a0aa5ef8
nsmzoneli <nsmzoneli@gmail.com>
2026-08-10 11:10:26 +0200

    added test for finishGame determining correct winner

--- backend/tests/unit/usecases/finish-game.unit.test.ts
+++ backend/tests/unit/usecases/finish-game.unit.test.ts
@@ -92,5 +92,53 @@ describe('FinishGame', () => {
             expect(match_stats_repo.saveStats).toHaveBeenCalledTimes(2);
 
         });
+
+        it('determines the winner as the player with more correct answers', async () => {
+            world.getMatchComponent.mockReturnValue({
+                submissions: new Map([
+                    ['player-a::q1', 1],
+                    ['player-a::q2', 2],
+                    ['player-b::q1', 3]
+                ])
+            });
+
+            //copied from above
+            const started = new Date('2026-01-01T00:00:00Z');
+            const submitted = new Date('2026-01-01T00:00:05Z');
+
+            //copied from above
+            world.getSubmissionComponent.mockImplementation((entity: number) => {
+                const data: Record<number, any> ={
+                    1: { correct: true, submitted_at: submitted, started_at: started },
+                    2: { correct: true, submitted_at: submitted, started_at: started },
+                    3: { correct: false, submitted_at: submitted, started_at: started },
+                };
+                return data[entity];
+            });
+
+            //cpoied from above
+            match_result_service.finaliseMatch.mockResolvedValueOnce({
+                players: [
+                    { user_id: 'player-a', eloEffect: 16 },
+                    { user_id: 'player-b', eloEffect: -16 }
+                ]
+            });
+
+            //copied from above
+            await finish_game.execute(match_id, player_ids, GameType.ranked, pair_id);
+
+            expect(match_result_service.finaliseMatch).toHaveBeenCalledWith(
+                db_match_id,
+                'player-a',
+                'player-b',
+                true,
+                expect.arrayContaining([
+                    expect.objectContaining({ user_id: 'player-a', correctness: 2}),
+                    expect.objectContaining({ user_id: 'player-b', correctness: 0})
+                ])
+            );
+        });
+
+        
     });
 });
