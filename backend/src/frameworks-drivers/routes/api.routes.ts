ab9969f5a2c1878c628e371f74c84076259b2ff1
nsmzoneli <nsmzoneli@gmail.com>
2026-08-10 03:29:21 +0200

    updated router to match history controllers

--- backend/src/frameworks-drivers/routes/api.routes.ts
+++ backend/src/frameworks-drivers/routes/api.routes.ts
@@ -1,21 +1,28 @@
 import { Router } from 'express';
 import { EloRatings } from 'src/entities/db-entities/elo.entities';
 import { Users } from 'src/entities/db-entities/user.entities';
+import { Matches, MatchLog } from 'src/entities/db-entities/match.entities';
+import { MatchStats } from 'src/entities/db-entities/match-stats.entities';
 import {
   getUserElo,
 } from 'src/interface-adapters/controllers/elo.controllers';
 import { getUserStat } from 'src/interface-adapters/controllers/user.controllers';
 import { EloRepository } from 'src/interface-adapters/repositories/elo.repository';
 import { UserRepository } from 'src/interface-adapters/repositories/user.repository';
-
+import { MatchHistoryRepository } from 'src/interface-adapters/repositories/match-history.repository';
+import { requireAuth } from 'src/interface-adapters/auth/auth.service';
 
 import { AppDataSource } from '../config/data-source';
+import { getMatchDetails, getMatchHistory } from 'src/interface-adapters/controllers/match-history.controllers';
 
 const router = Router();
 
 const user_repo = new UserRepository(AppDataSource.getRepository(Users))
 const elo_repo = new EloRepository(AppDataSource.getRepository(EloRatings))
+const match_history_repo = new MatchHistoryRepository( AppDataSource.getRepository(Matches), AppDataSource.getRepository(MatchLog), AppDataSource.getRepository(MatchStats));
 router.get('/elo-get', getUserElo(elo_repo));
+router.get('/matches', getMatchHistory(match_history_repo));
+router.get('/matches/:match_id', getMatchDetails(match_history_repo));
 
 // user routes
 router.get('/:stat', getUserStat(user_repo)); // this must be last, it's a generic function that fetches any attribute directly in the users table
