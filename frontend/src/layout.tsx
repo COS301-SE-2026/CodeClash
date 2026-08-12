6f1555c1044f11e5ff57f2c1bfac907b2976227f
TaskeenAbdoola <u22698681@tuks.co.za>
2026-08-12 21:42:15 +0200

    Removed double call to Outlet

--- frontend/src/layout.tsx
+++ frontend/src/layout.tsx
@@ -33,7 +33,6 @@ export default function Layout() {
                         </main>
                     </div>
             </SidebarInset>
-            <Outlet />
         </SidebarProvider>
     )
 }
