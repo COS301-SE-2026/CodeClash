5c8aae9cc0f6c060dbcc1e774954c6bc92a9c90a
morganthegirlboss <u24910882@tuks.co.za>
2026-08-17 11:47:06 +0200

    starting on testing the rank controller

---
+++ backend/tests/unit/controllers/rank.controller.test.ts
@@ -0,0 +1,14 @@
+import {describe, expect, it, vi} from 'vitest';
+import {getUserRank} from "../../../src/interface-adapters/controllers/rank.controllers"
+
+describe("Rank Controller Test", () => {
+
+    it("Returns rank for valid user", async () => {
+
+        
+    })
+
+
+
+
+})
