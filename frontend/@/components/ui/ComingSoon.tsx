7130dc003d8c14e1fcce434f74113c4514f2828a
TaskeenAbdoola <u22698681@tuks.co.za>
2026-08-19 11:02:20 +0200

    Added a fullscreen flag so that the content doesnt render so low when the header and sidebar is open for Tournaments

--- frontend/@/components/ui/ComingSoon.tsx
+++ frontend/@/components/ui/ComingSoon.tsx
@@ -1,21 +1,23 @@
 //This will be a coming soon page to be used for our wow factors that arent implemented yet - its just so that when user clicks tournaments for example, its not blank
 
-import { Telescope } from "lucide-react";
+import { Telescope} from "lucide-react";
 import React from "react";
 
 type ComingSoonProps = {
     title?: string;
     description?: string;
     icon?: React.ComponentType<{size?: number; classname?: string}>;
+    fullscreen?: boolean;
 };
 
 const ComingSoon = ({
     title = "Coming Soon!",
     description = "This feature is still being built by the CodeClash engineers.",
     icon: Icon = Telescope,
+    fullscreen = true
 }: ComingSoonProps) => {
     return (
-        <div className="relative min-h-screen flex items-center justify-center p-8 overflow-hidden">
+        <div className={`relative ${fullscreen? 'min-h-screen' : 'h-full mt-28'} flex items-center justify-center overflow-hidden`}>
             <div className="relative z-10 flex flex-col items-center text-center gap-6 mx-w-md">
                 <div className="relative flex items-center justify-center">
                     <div className="absolute w-40 h-40 rounded-full animate-glow"
@@ -26,8 +28,8 @@ const ComingSoon = ({
                 </div>
 
                 <div>
-                    <h1 className="text-3xl font-black text-primart-text mb-3">{title}</h1>
-                    <p className="text-muted leading-relaxed">{description}</p>
+                    <h1 className="text-l font-black text-primary-text mb-3">{title}</h1>
+                    <p className="text-muted text-xsm leading-relaxed">{description}</p>
                 </div>
             </div>
         </div>
