7bf3e52b79d6d78b1971990eeba881ec918c32d7
nsmzoneli <nsmzoneli@gmail.com>
2026-08-20 23:51:45 +0200

    Revert "created the achievement entity file"
    This reverts commit 765ef2015bbdeaf4bbc3903455139f8bc81bbdd9.

--- backend/src/entities/db-entities/friendship.entities.ts
+++
@@ -1,5 +0,0 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne} from 'typeorm';
-import { Users } from './user.entities';
-
-export type FriendshipStatus = 'pending' | 'accepted' | 'declined' | 'blocked';
-
