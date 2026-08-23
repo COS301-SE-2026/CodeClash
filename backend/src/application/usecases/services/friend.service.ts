12ed391d28ce8bbd7562662e9071e4dc72c4c848
nsmzoneli <nsmzoneli@gmail.com>
2026-08-23 19:02:35 +0200

    added half implementation to friend service

--- backend/src/application/usecases/services/friend.service.ts
+++ backend/src/application/usecases/services/friend.service.ts
@@ -8,18 +8,22 @@ export class FriendService {
     ) {}
 
     async getFriends(user_id: string): Promise<FriendDTO[]> {
-
+        return this.friend_repo.getFriends(user_id);
     }
 
     async getFriendRequests(user_id: string, type: 'sent' | 'received'): Promise<FriendRequestDTO[]> {
-
+        return this.friend_repo.getFriendRequests(user_id, type);
     }
 
     async sendFriendRequests(requester_id: string, receiver_id: string): Promise<void> {
+        if (requester_id === receiver_id) throw new Error('Cannot send friend request to yourself');
 
+        return this.friend_repo.sendFriendRequest(requester_id, receiver_id);
     }
 
     async respondToRequest(friendship_id: string, status: 'accepted' | 'declined'): Promise<void> {
-        
+        return this.friend_repo.respondToRequest(friendship_id, status);
     }
+
+    
 }
