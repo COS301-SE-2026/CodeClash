440508f5b136ace98a8514ac00fb0bd8e54bf0cd
nsmzoneli <nsmzoneli@gmail.com>
2026-08-23 19:21:29 +0200

    added remaining implementation for friend service

--- backend/src/application/usecases/services/friend.service.ts
+++ backend/src/application/usecases/services/friend.service.ts
@@ -25,5 +25,25 @@ export class FriendService {
         return this.friend_repo.respondToRequest(friendship_id, status);
     }
 
+    async removeFriend(friendship_id: string): Promise<void> {
+        return this.friend_repo.removeFriend(friendship_id);
+    }
+
+    async createInvite(sender_id: string): Promise<FriendInviteDTO> {
+        const invite_code = randomBytes(16).toString('hex');
+        const expires_at = new Date(Date.now() + 1 * 60 * 60 * 1000); // 1 hour
+        return this.friend_repo.createInvite(sender_id, invite_code, expires_at);
     
+    }
+
+    async acceptInvite(invite_code: string, receiver_id: string): Promise<void> {
+        const invite = await this.friend_repo.getInviteByCode(invite_code);
+        if(!invite) throw new Error('Invite not found');
+        if (new Date() > invite.expires_at) throw new Error('Invite has expired');
+        // TODO sender_id needs to come from invite. Might have to extend FriendInviteDTO
+     }
+
+     async getFriendCount(user_id: string): Promise<number> {
+        return this.friend_repo.getFriendCount(user_id);
+     }
 }
