98f903f07469beb2874834c8a53da24b175c4d03
nsmzoneli <nsmzoneli@gmail.com>
2026-08-23 19:33:14 +0200

    added the structure for the achievement and friend controllers

---
+++ backend/src/interface-adapters/controllers/achievement.controllers.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { AchievementService } from "src/application/usecases/services/achievement.service";
+
+export const getAllAchievements = (service: AchievementService) =>
+    async (req: Request, res: Response): Promise<void> => {
+        try{
+
+        }catch (error) {
+
+        }
+    };
+
+// copied structure
+export const getUserAchievements = (service: AchievementService) => 
+    async (req: Request, res: Response): Promise<void> => {
+        try{
+
+        }catch (error) {
+
+        }
+    };
+
+    
---
+++ backend/src/interface-adapters/controllers/friend.controllers.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import { FriendService } from "src/application/usecases/services/friend.service";
+
+export const getFriends = (service: FriendService) => 
+    async (req: Request, res: Response): Promise<void> => {
+        try{
+
+        }catch (error) {
+            
+        }
+    };
+
+export const getFriendRequests = (service: FriendService) => 
+    async (req: Request, res: Response): Promise<void> => {
+        try{
+
+        }catch (error) {
+            
+        }
+    };
+
+export const sendFriendRequest = (service: FriendService) =>
+    async (req: Request, res: Response): Promise<void> => {
+        try{
+
+        }catch (error: any) {
+
+        }
+    };
+
+export const respondToFriendRequest = (service: FriendService) => 
+    async (req: Request, res: Response): Promise<void> => {
+        try{
+
+        }catch (error) {
+
+        }
+    };
+
+// copied structure
+export const removeFriend = (service: FriendService) => 
+    async (req: Request, res: Response): Promise<void> => {
+        try{
+
+        }catch (error) {
+
+        }
+    };
+
+// copied structure
+export const createInvite = (service: FriendService) => 
+    async (req: Request, res: Response): Promise<void> => {
+        try{
+
+        }catch (error) {
+
+        }
+    };
