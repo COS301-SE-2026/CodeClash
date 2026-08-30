0967378cf75774c14b9ea4f48ee122a87f14d3f1
TaskeenAbdoola <u22698681@tuks.co.za>
2026-08-30 23:26:29 +0200

    Mock data for AChievements view model as there is no api endpoints that I know of yet

---
+++ frontend/src/ViewModels/AchievementsViewModel.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { achievementContent } from "src/Models/AchievementsModel";
+import type { Achievements, AchievementsContent, Earned } from "src/Models/AchievementsModel";
+
+//This system needs an api and integration, so I am using mock data
+
+const MOCK_ACHIEVEMENTS: Achievements[] = [
+    {
+        id: 'Ach1',
+        name: 'Ach1 Name',
+        description: 'Ach1 description - what does this badge do and how it was earned...',
+        icon: 'trophy'
+    },
+    {
+        id: 'Ach2',
+        name: 'Ach2 Name',
+        description: 'Ach2 description - what does this badge do and how it was earned...',
+        icon: 'flame'
+    },
+    {
+        id: 'Ach3',
+        name: 'Ach3 Name',
+        description: 'Ach3 description - what does this badge do and how it was earned...',
+        icon: 'zap'
+    },
+    {
+        id: 'Ach4',
+        name: 'Ach4 Name',
+        description: 'Ach4 description - what does this badge do and how it was earned...',
+        icon: 'medal'
+    }
+]
+
+const MOCKED_EARNED: Earned[] = [
+    {
+        id: 'AchEarned1',
+        earnedAt: new Date(Date.now() - 1000 * 60 * 60 *24 * 20).toISOString()
+    },
+    {
+        id: 'AchEarned2',
+        earnedAt: new Date(Date.now() - 1000 * 60 * 60 *24 * 6).toISOString()
+    }
+]
