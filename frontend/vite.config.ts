01f493491d7922001ed84c18bec3811244df27fd
nsmzoneli <nsmzoneli@gmail.com>
2026-09-02 16:00:29 +0200

    bug fix attempt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 import { defineConfig } from 'vitest/config'
+import { loadEnv } from 'vite'
 dotenv.config();
 
 export default defineConfig({
@@ -22,5 +23,13 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['@monaco-aditor/react', 'mathlive'],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   }
 })
