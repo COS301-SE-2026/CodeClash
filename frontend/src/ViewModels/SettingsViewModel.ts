dd157210709e0e28e98acefeaee6340621ff820b
TaskeenAbdoola <u22698681@tuks.co.za>
2026-09-03 08:41:17 +0200

    settings vm

--- frontend/src/ViewModels/SettingsViewModel.ts
+++ frontend/src/ViewModels/SettingsViewModel.ts
@@ -1,36 +1,10 @@
-//Setting page view model - will toggle light and dark mode from here for now, later on will add other controls like maybe notifications, sounds etc
-
-import { useEffect, useState } from "react";
-
-export type Theme = 'dark' | 'light';
-const themeKey = 'codeclash-themes';
+import { useTheme } from "src/context/ThemeContext";
 
 export const SettingsViewModelFunc = () => {
-    const [theme, setTheme] = useState<Theme>(() => {
-        if (typeof window === 'undefined') {
-            return 'dark';
-        }
-        const stored = window.localStorage.getItem(themeKey) as Theme | null;
-        return stored ?? 'dark';
-    })
-
-    useEffect(() => {
-        const root = document.documentElement;
-        if (theme === 'light') {
-            root.classList.add('light');
-        }
-        else {
-            root.classList.remove('light');
-        }
-    }, [theme])
-    
-    const toggleTheme = () => {
-        setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'));
-    }
+    const {isLight, toggleTheme} = useTheme();
 
     return {
-        theme,
-        isLight: theme === 'light',
-        toggleTheme,
+        isLight,
+        toggleTheme
     }
 }
