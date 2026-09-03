2fa6c0b6a54e668684692fb013ba082d9e295836
nsmzoneli <nsmzoneli@gmail.com>
2026-09-04 01:07:12 +0200

    updated userContext for streaks

--- frontend/src/context/User/UserContext.tsx
+++ frontend/src/context/User/UserContext.tsx
@@ -16,7 +16,9 @@ export const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     const [league, setLeague] = useState('');
     const [rank, setRank] = useState(0);
     const { user, token, isLoading } = useAuth();
- 
+    const [current_streak, setCurrentStreak] = useState<number>(0);
+    const [winning_streak, setWinningStreak] = useState<number>(0);
+
     const userId = user?.userId ?? ""
     const username = user?.username ?? '';
 
@@ -124,12 +126,43 @@ export const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
 
     }
 
+    const getCurrentStreak =  async () => {
+        if (!token) {
+            setError('Missing or Invalid Token');
+            return;
+        }
+        try{
+            const res = await axios.get(url.concat('user/current_streak'), {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            if (res.status === 200) setCurrentStreak(res.data.current_streak);
+        }catch (error) {
+            console.error('getCurrentRank failed', error);
+        }
+    };
+
+    const getWinningStreak =  async () => {
+        if (!token) {
+            setError('Missing or Invalid Token');
+            return;
+        }
+        try{
+            const res = await axios.get(url.concat('user/winning_streak'), {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            if (res.status === 200) setWinningStreak(res.data.winning_streak);
+        }catch (error) {
+            console.error('getCurrentRank failed', error);
+        }
+    };
     const refresh = async () =>{
         await Promise.all([
             getElo(),
             getAvatarUrl(),
             getLeague(),
-            getRank()
+            getRank(),
+            getCurrentStreak(),
+            getWinningStreak()
         ])
     }
 
@@ -144,6 +177,8 @@ export const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
                 getLeague(),
                 getElo(),
                 getRank(),
+                getCurrentStreak(), // copied from above
+                getWinningStreak()
             ]);
         }
 
@@ -152,8 +187,8 @@ export const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
 
 
     const value = useMemo(() => ({
-        username, elo, avatar, error, league, userId, refresh, rank
-    }), [username, elo, avatar, error, league, userId, rank])
+        username, elo, avatar, error, league, userId, refresh, rank, current_streak, winning_streak
+    }), [username, elo, avatar, error, league, userId, rank, current_streak, winning_streak])
 
     return (
         <UserContext.Provider
