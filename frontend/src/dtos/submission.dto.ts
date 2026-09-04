0d16832b2cbe4f354bee545cea8bf67cd5324cf1
Ntuuu <apathylely@gmail.com>
2026-09-04 05:56:26 +0200

    Added the other DTO's

--- frontend/src/dtos/submission.dto.ts
+++ frontend/src/dtos/submission.dto.ts
@@ -4,3 +4,13 @@ export interface SubmissionDTO{
     result: boolean,
     life_update: number
 }
+
+export interface MathsSubmissionDTO {
+    answer: string
+}
+
+export interface ProgSubmissionDTO {
+    source_code: string,
+    language_id: number,
+    stdin: string | null,
+}
