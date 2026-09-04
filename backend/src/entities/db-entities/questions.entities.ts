a485e6aa8db421385cfec8526eb921fe91f23d94
Ntuuu <apathylely@gmail.com>
2026-09-04 02:05:04 +0200

    Comment for some clarification ig

--- backend/src/entities/db-entities/questions.entities.ts
+++ backend/src/entities/db-entities/questions.entities.ts
@@ -47,7 +47,7 @@ export class Questions {
     time_limit!: string
 
     @Column({ nullable: true, type: "enum", enum: AnswerFormat })
-    answer_format!: AnswerFormat | null
+    answer_format!: AnswerFormat | null // for prog matches, a prog match wouldnt like, need a numerical answer format requirement
   
     @Column({ nullable: true, type: "integer" })
     answer_precision!: number | null }
