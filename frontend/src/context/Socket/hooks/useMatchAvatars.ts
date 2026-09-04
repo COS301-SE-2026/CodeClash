fbdb665bba0f35ab2751dee90d316f7fc00659ea
morganthegirlboss <u24910882@tuks.co.za>
2026-09-04 04:51:59 +0200

    realised current set up for sockets and handlers/providers would not work for seeing opponent's avatar in a match as is - creating new hook for the match page, continued

--- frontend/src/context/Socket/hooks/useMatchAvatars.ts
+++ frontend/src/context/Socket/hooks/useMatchAvatars.ts
@@ -2,4 +2,15 @@ import {useState, useEffect} from 'react';
 import { useSocket } from './useSocket';
 
 
-export function useMatchAvatars
+export function useMatchAvatars({pair_id, myPlayerId, myPose, myColour}) {
+    const [opponent, setOpponent] = useState(null);
+    const socket = useSocket();
+
+    useEffect(() => {
+        if (!pair_id){
+            return;
+        }
+
+        socket.emit('match_join', {pair_id, pose: myPose, colour: myColour});
+    })
+}
