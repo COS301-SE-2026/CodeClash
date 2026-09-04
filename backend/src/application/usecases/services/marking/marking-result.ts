e6bf819f72008c9b052ed27f4dfdf2ea73964c4e
Ntuuu <apathylely@gmail.com>
2026-09-04 04:58:15 +0200

    Added: imports and fixed function signatures and added null check

--- backend/src/application/usecases/services/marking/marking-result.ts
+++ backend/src/application/usecases/services/marking/marking-result.ts
@@ -3,7 +3,18 @@
 
 export class MarkingResult{
 
-    constructor(){
+  private constructor(
+    public readonly correct: boolean,
+    public readonly reason: string,
+    ){
         
+    }
+
+    static right(reason = "correct"): MarkingResult {
+        return new MarkingResult(true, reason);
+    }
+
+    static wrong(reason: string): MarkingResult {
+        return new MarkingResult(false, reason);
     }
 }
