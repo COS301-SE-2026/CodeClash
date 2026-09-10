0e8882a3307b7f505c272a2420b7cb01bd82df18
TaskeenAbdoola <u22698681@tuks.co.za>
2026-09-10 21:43:05 +0200

    updated settings viev model to accomodate for the themes - im not sure if it will remain in settings but this helps me test the colors

--- frontend/src/ViewModels/SettingsViewModel.ts
+++ frontend/src/ViewModels/SettingsViewModel.ts
@@ -1,10 +1,38 @@
 import { useTheme } from "src/context/ThemeContext";
+import type { Theme } from "src/context/ThemeContext";
+
+export interface ThemesContent {
+    id: Theme;
+    label: string;
+    swatch: string; //this will be used for a preview of what the primary color for the theme is
+}
+
+const Themes: ThemesContent[] = [
+    {
+        id: 'dark',
+        label: 'Cosmo',
+        swatch: '#c0395a'
+    },
+    {
+        id: 'light',
+        label: 'Cosmo Light',
+        swatch: '#c0395a'
+    },
+    {
+        id: 'frost',
+        label: 'Frost',
+        swatch: '#2f8fe0'
+    },
+]
 
 export const SettingsViewModelFunc = () => {
-    const {isLight, toggleTheme} = useTheme();
+    const {isLight, toggleTheme, theme, setTheme} = useTheme();
 
     return {
         isLight,
-        toggleTheme
+        toggleTheme,
+        theme,
+        setTheme,
+        themes: Themes
     }
 }
