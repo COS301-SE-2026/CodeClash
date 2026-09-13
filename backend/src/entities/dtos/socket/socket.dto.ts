68fb3c8843082a2c5dd196dec3a58997a8b34562
Swe-yy <u24584585@tuks.co.za>
2026-09-13 22:39:40 +0200

    implemented socket dispatcher

---
+++ backend/src/entities/dtos/socket/socket.dto.ts
@@ -0,0 +1,13 @@
+// Generic types for socket handling 
+export type SocketAck<T = unknown> =
+    {
+        ok: true;
+        data: T;
+    } |
+    {
+        ok: false;
+        error: string
+    };
+
+export type SocketAckCallback<T> = (res: SocketAck<T>) => void;
+
