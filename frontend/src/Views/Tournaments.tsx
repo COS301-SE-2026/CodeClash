156289ac3bea5b6b1943cd490118eab19b665c68
morganthegirlboss <u24910882@tuks.co.za>
2026-09-15 23:51:00 +0200

    making some initial design changes to tournaments page

--- frontend/src/Views/Tournaments.tsx
+++ frontend/src/Views/Tournaments.tsx
@@ -10,14 +10,17 @@ const Tournaments = () => {
         <div className="w-full min-h-flex">
             <div className="h-full w-full flex grid grid-rows-3">
                 <div className="flex grid grid-cols-2 h-full w-full">
-                    <h1 className="font-font font-semibold mx-auto font-3xl">Tournaments</h1>
-                    <TournamentButton className="w-[35%] h-[110%]">
-                        <PlusIcon/>
-                        <h2 className="font-font font-semibold mx-auto font-xl">Host Tournament</h2>
+                    <h1 className="font-font font-semibold my-auto text-3xl">Tournaments</h1>
+                    <TournamentButton className="w-[40%] h-[75%]">
+                        <div className="grid grid-cols-2 w-full">
+                            <PlusIcon/>
+                            <h2 className="font-font font-semibold mx-auto text-xl">Host Tournament</h2>
+                        </div>
                     </TournamentButton>
+                    <h2 className="font-font text-xl">Join Tournaments to Compete With Up to 30 Players! Invite Friends to Join in on the Fun!</h2>
                 </div>
-            </div>
-           
+
+            </div> 
         </div>
     )
 }
