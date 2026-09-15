c9deb3463a27991278afeb53bde4b95df58777bc
morganthegirlboss <u24910882@tuks.co.za>
2026-09-15 23:44:16 +0200

    i lowk forgot to commit so far, but i have come far with creating different componenets for the tournaments page and for starting to design it

--- frontend/src/Views/Tournaments.tsx
+++ frontend/src/Views/Tournaments.tsx
@@ -1,14 +1,23 @@
 //This is a wow factor - i am just calling the coming soon component so the page isnt blank when clicked from dashboard
+//import TournamentCard from "@/components/ui/TournamentCard"
 import TournamentButton from "@/components/ui/TournamentButton"
 // import ComingSoon from "@/components/ui/ComingSoon";
+import "../styles/global.css"
+import {PlusIcon} from "lucide-react"
 
 const Tournaments = () => {
     return (
-        <div className="min-h-flex">
-            {/* <ComingSoon
-                fullscreen={false}/>
-         */}
-        <TournamentButton/>
+        <div className="w-full min-h-flex">
+            <div className="h-full w-full flex grid grid-rows-3">
+                <div className="flex grid grid-cols-2 h-full w-full">
+                    <h1 className="font-font font-semibold mx-auto font-3xl">Tournaments</h1>
+                    <TournamentButton className="w-[35%] h-[110%]">
+                        <PlusIcon/>
+                        <h2 className="font-font font-semibold mx-auto font-xl">Host Tournament</h2>
+                    </TournamentButton>
+                </div>
+            </div>
+           
         </div>
     )
 }
