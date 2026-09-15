04a728abaa2b8be9b20ced1e816657792266a120
morganthegirlboss <u24910882@tuks.co.za>
2026-09-15 21:46:36 +0200

    created tournament button component

--- frontend/src/Views/Tournaments.tsx
+++ frontend/src/Views/Tournaments.tsx
@@ -1,12 +1,14 @@
 //This is a wow factor - i am just calling the coming soon component so the page isnt blank when clicked from dashboard
-
-import ComingSoon from "@/components/ui/ComingSoon";
+import TournamentButton from "@/components/ui/TournamentButton"
+// import ComingSoon from "@/components/ui/ComingSoon";
 
 const Tournaments = () => {
     return (
         <div className="min-h-flex">
-            <ComingSoon
+            {/* <ComingSoon
                 fullscreen={false}/>
+         */}
+        <TournamentButton/>
         </div>
     )
 }
