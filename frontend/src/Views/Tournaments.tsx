d4b8bb346c6a4f62de2f4b627cfd2702ed814720
morganthegirlboss <u24910882@tuks.co.za>
2026-09-16 00:58:18 +0200

    working on main card on tournament page

--- frontend/src/Views/Tournaments.tsx
+++ frontend/src/Views/Tournaments.tsx
@@ -1,5 +1,5 @@
 //This is a wow factor - i am just calling the coming soon component so the page isnt blank when clicked from dashboard
-//import TournamentCard from "@/components/ui/TournamentCard"
+//import MathTournamentCard from "@/components/ui/TournamentCard"
 import TournamentButton from "@/components/ui/TournamentButton"
 import FilterButton from "@/components/ui/FilterButton"
 // import ComingSoon from "@/components/ui/ComingSoon";
@@ -12,21 +12,21 @@ const Tournaments = () => {
             <div className="h-full w-full flex grid grid-rows-3">
                 <div className="flex grid grid-cols-2 h-full w-full">
                     <h1 className="font-font font-semibold text-xl ml-3 mt-3">Tournaments</h1>
-                    <TournamentButton className="w-[40%] h-[60%] my-auto ml-50">
+                    <TournamentButton className="w-[40%] h-[60%] my-auto ml-70">
                         <div className="grid grid-cols-2 w-full">
                             <PlusIcon size={50} className="mb-4 ml-3 -mt-5"/>
-                            <h2 className="font-font font-semibold text-md mb-4 -ml-15 -mt-5">Host Tournament</h2>
+                            <h2 className="font-font font-semibold text-md mb-5 -ml-15 -mt-5">Host Tournament</h2>
                         </div>
                     </TournamentButton>
                     <h2 className="font-font text-sm -mt-3 ml-3">Join Tournaments to Compete With Up to 30 Players! Invite Friends to Join in on the Fun!</h2>
                 </div>
 
                 <div className="flex grid grid-cols-2 w-full">
-                    <div className="w-full">
+                    <div className="w-[110%]">
 
                     </div>
 
-                    <div className="mt-8 flex grid grid-cols-3 w-full">
+                    <div className="mt-6 flex grid grid-cols-3 w-full">
                         <FilterButton>Math</FilterButton>
                         <FilterButton>Programming</FilterButton>
                         <FilterButton>Both</FilterButton>
