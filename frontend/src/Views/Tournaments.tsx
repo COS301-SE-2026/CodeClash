b6850714c8a1e8c1c69d923269df0e3c58f2d13c
morganthegirlboss <u24910882@tuks.co.za>
2026-09-18 13:28:28 +0200

    added tournament id to cards

--- frontend/src/Views/Tournaments.tsx
+++ frontend/src/Views/Tournaments.tsx
@@ -1,5 +1,5 @@
 //This is a wow factor - i am just calling the coming soon component so the page isnt blank when clicked from dashboard
-import {MathTournamentCard} from "@/components/ui/TournamentCard"
+import {MathTournamentCard, ProgrammingTournamentCard} from "@/components/ui/TournamentCard"
 import TournamentButton from "@/components/ui/TournamentButton"
 import FilterButton from "@/components/ui/FilterButton"
 // import ComingSoon from "@/components/ui/ComingSoon";
@@ -8,17 +8,17 @@ import {PlusIcon, Search} from "lucide-react"
 
 const Tournaments = () => {
     return (
-        <div className="w-full min-h-flex">
-            <div className="h-full w-full flex grid grid-rows-3">
+        <div className="w-full max-h-screen">
+            <div className="w-full flex grid grid-rows-3">
                 <div className="flex grid grid-cols-2 h-full w-full">
                     <h1 className="font-font font-semibold text-6xl ml-6 mt-6">Tournaments</h1>
-                    <TournamentButton className="w-md h-[50%] mt-3 ml-[40%]">
+                    <TournamentButton className="w-md h-17 mt-3 ml-[40%]">
                         <div className="grid grid-cols-2 w-full">
                             <PlusIcon size={60} className="mb-4 ml-3 -mt-5.5"/>
                             <h2 className="font-font font-semibold text-[40px] mb-5 -ml-30 -mt-5">Host Tournament</h2>
                         </div>
                     </TournamentButton>
-                    <h2 className="font-font text-sm -mt-5 ml-6 w-full">Join Tournaments to Compete With Up to 30 Players! Invite Friends to Join in on the Fun!</h2>
+                    <h2 className="font-font text-sm ml-6 w-full">Join Tournaments to Compete With Up to 30 Players! Invite Friends to Join in on the Fun!</h2>
                 </div>
 
                 <div className="flex grid grid-cols-2 w-full">
@@ -26,11 +26,11 @@ const Tournaments = () => {
 
                     </div>
 
-                    <div className="mt-8 flex flex-row gap-10 ml-[45%]">
+                    <div className=" flex flex-row gap-10 ml-[45%]">
                         <FilterButton className="">
                             <h1 className="text-primary font-semibold text-[90%]">Math</h1>
                         </FilterButton>
-                        <FilterButton className="w-[40%]">
+                        <FilterButton className="w-40">
                             <h1 className="text-primary font-semibold text-[90%]">Programming</h1>
                         </FilterButton>
                         <FilterButton>
@@ -39,7 +39,9 @@ const Tournaments = () => {
                     </div>
                 </div>
 
-                <div className="overflow-y-auto -mt-10 w-full">
+                <div className="overflow-y-auto w-full flex flex-col gap-9">
+                    <MathTournamentCard/>
+                    <ProgrammingTournamentCard/>
                     <MathTournamentCard/>
                 </div>
             </div> 
