368c6a8909793859b251642334a6f6228f3d8745
morganthegirlboss <u24910882@tuks.co.za>
2026-09-18 15:34:01 +0200

    adding top card to tournament waiting page

--- frontend/src/Views/TournamentsWaiting.tsx
+++ frontend/src/Views/TournamentsWaiting.tsx
@@ -1,13 +1,21 @@
 import "../styles/global.css"
+import { Card } from "../../@/components/ui/card"
 
 
 const TournamentsWaiting = () => {
     return(
         <div className="w-full max-h-screen">
             <div className="flex flex-col">
-                <div className="bg-card border-[var(--button-tournament-secondary)] border-[0.5px] flex flex-col">
+                <Card className="bg-card border-[var(--button-tournament-secondary)] border-[0.5px] flex flex-col ">
+                    <div className="flex flex-row">
+                        <h1 className="font-font font-semibold text-2xl">Tournament Title</h1>
+                        <Card className="bg-card border-[var(--button-tournament-secondary)] border-[0.5px]">
+                            <div className="flex flex-row">
 
-                </div>
+                            </div>
+                        </Card>
+                    </div>
+                </Card>
             </div>
         </div>
     )
