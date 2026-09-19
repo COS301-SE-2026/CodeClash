814f246a9ddc0607c518c419174def84d964512f
morganthegirlboss <u24910882@tuks.co.za>
2026-09-19 02:05:54 +0200

    starting on new match card component

---
+++ frontend/@/components/ui/MatchCard.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "../../../src/styles/global.css"
+import { Card } from "../ui/card"
+
+
+interface MatchCardProps{
+    children?: React.ReactNode;
+    className?: string;
+}
+
