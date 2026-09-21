dca7d4810431c1dbf539bc616fccceee59bb02d0
nsmzoneli <nsmzoneli@gmail.com>
2026-09-21 15:54:40 +0200

    added implementation for the DTO conversion in shop repo

--- backend/src/interface-adapters/repositories/shop.repository.ts
+++ backend/src/interface-adapters/repositories/shop.repository.ts
@@ -19,19 +19,37 @@ export class ShopRepository implements IShopRepository {
 
     private toItemDTO(item: ShopItem): ShopItemDTO {
         return {
-
-        } as ShopItemDTO;
+            shop_item_id: item.shop_item_id,
+            category: item.category,
+            name: item.name,
+            description: item.description,
+            price: item.price,
+            rarity: item.rarity,
+            metadata: item.metadata,
+            created_at: item.created_at
+        } as unknown as ShopItemDTO;
     }
 
     private toWalletDTO(wallet: Wallet): WalletDTO {
         return {
-
+            wallet_id: wallet.wallet_id,
+            user_id: wallet.user.user_id,
+            balance: wallet.balance,
+            updated_at: wallet.updated_at
         };
     }
 
     private toEquippedDTO(equipped: EquippedItems): EquippedItemsDTO {
         return {
-
+            user_id: equipped.user.user_id,
+            avatar: equipped.avatar? this.toItemDTO(equipped.avatar) : null,
+            top: equipped.top? this.toItemDTO(equipped.top) : null,
+            bottom: equipped.bottom? this.toItemDTO(equipped.bottom) : null,
+            one_piece: equipped.one_piece? this.toItemDTO(equipped.one_piece) : null,
+            shoes: equipped.shoes? this.toItemDTO(equipped.shoes) : null,
+            hat: equipped.hat? this.toItemDTO(equipped.hat) : null,
+            powerup: equipped.powerup? this.toItemDTO(equipped.powerup) : null,
+            updated_at: equipped.updated_at
         };
     }
 
