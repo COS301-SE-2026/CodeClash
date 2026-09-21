6e03e64a45e2fc352fd9fbf82f796190d87a8907
nsmzoneli <nsmzoneli@gmail.com>
2026-09-21 13:54:23 +0200

    created equipped items and user items dtos

--- backend/src/entities/dtos/shop/equipped-items.dto.ts
+++ backend/src/entities/dtos/shop/equipped-items.dto.ts
@@ -1,2 +1,23 @@
 import { ShopItemDTO } from './shop.dto';
 
+export interface EquippedItemsDTO {
+    user_id: string;
+    avatar: ShopItemDTO | null;
+    top: ShopItemDTO | null;
+    bottom: ShopItemDTO | null;
+    one_piece: ShopItemDTO | null;
+    shoes: ShopItemDTO | null;
+    hat: ShopItemDTO | null;
+    powerup: ShopItemDTO | null;
+    updated_at: Date;
+}
+
+export interface UpdatedEquippedDTO {
+    avatar_item_id?: string;
+    top_id?: string;
+    bottom_id?: string;
+    one_piece_id?: string;
+    shoes_id?: string;
+    hat_id?: string;
+    powerup_item_id?: string; 
+}
