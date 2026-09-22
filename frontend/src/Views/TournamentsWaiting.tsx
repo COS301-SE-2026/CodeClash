fa6a4ce40f84c14a386f6639a55568288cfa154e
morganthegirlboss <u24910882@tuks.co.za>
2026-09-22 04:41:11 +0200

    making more adjustments on tournaments waiting page

--- frontend/src/Views/TournamentsWaiting.tsx
+++ frontend/src/Views/TournamentsWaiting.tsx
@@ -19,15 +19,15 @@ const TournamentsWaiting = () => {
                     <div className="flex flex-row w-full -mt-1">
                         <h1 className="font-font font-semibold text-[2.5rem] ml-5 -mt-3">Tournament Title</h1>
 
-                        <div className="gap-5 flex flex-row ml-auto mr-6">
-                            <MatchCard className="w-[50%] min-h-[40%] hover:opacity-90 hover:scale-105 transition-transform duration-300">
+                        <div className="gap-5 flex flex-row ml-auto mr-6 -mt-5">
+                            <MatchCard className="w-[60%] min-h-[40%] hover:opacity-90 hover:scale-105 transition-transform duration-300">
                                 <div className="flex flex-row gap-2">
                                     <LogOut size={20} className="text-muted-text ml-3 -mt-2.5"/>
-                                    <h2 className="font-font font-semibold text-[0.8rem] text-muted-text -ml-3 px-3 -mt-2.5">Leave Waiting Room</h2>
+                                    <h2 className="font-font font-semibold text-[0.9rem] text-muted-text -ml-3 px-3 -mt-2.5">Leave Waiting Room</h2>
                                 </div>
                             </MatchCard>
-                            <TournamentButton className="items-center h-[65%] justify-center px-2 py-2 rounded-sm">
-                                <div className="flex flex-row mt-1">
+                            <TournamentButton className="items-center h-[67%] justify-center px-2 py-2 rounded-sm">
+                                <div className="flex flex-row my-auto">
                                     <Rocket size={25} className="ml-1"/>
                                     <div className="font-font font-semibold text-[1.1rem] ml-2 mr-1">Start Match</div>
                                 </div>
@@ -38,14 +38,14 @@ const TournamentsWaiting = () => {
                     <div className="flex flex-col -mt-1">
                         <div className="flex flex-row">
                             <div className="font-font font-semibold text-[1.2rem] text-primary ml-6">Room Capacity</div>
-                            <div className="ml-auto font-semibold mr-6 text-[1.5rem]">4/8 Players</div>
+                            <div className="ml-auto font-semibold mr-6 text-[1.1rem]">4/8 Players</div>
                         </div>
-                        <Progress value={50} className="w-[98%] mx-auto mt-2"/>
+                        <Progress value={50} className="w-[95%] mx-auto mt-2"/>
                     </div>
 
                     <div className="flex flex-row">
-                        <Timer size={50} className="text-muted-text ml-5 mt-3"/>
-                        <div className="font-font font-semibold text-sm mt-6 ml-2">Time Until Start:</div>
+                        <Timer size={35} className="text-muted-text ml-5"/>
+                        <div className="font-font font-semibold text-xs mt-3 ml-2">Time Until Start:</div>
                     </div>
                 </Card>
 
