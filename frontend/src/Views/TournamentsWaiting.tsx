a36de31bfcea82ae66bc0700db4a922b9b3a263a
morganthegirlboss <u24910882@tuks.co.za>
2026-09-22 04:58:57 +0200

    finished tournaments waiting page for now

--- frontend/src/Views/TournamentsWaiting.tsx
+++ frontend/src/Views/TournamentsWaiting.tsx
@@ -1,5 +1,4 @@
 import "../styles/global.css"
-import { Card } from "../../@/components/ui/card"
 import {LogOut, Rocket, Timer, PlusIcon, UserRoundPlus} from "lucide-react"
 import TournamentButton from "@/components/ui/TournamentButton"
 import { Progress } from "@/components/ui/progress"
@@ -12,7 +11,7 @@ const TournamentsWaiting = () => {
     return(
         <div className="w-full min-h-screen overflow-hidden relative">
             <div className="flex flex-col max-w-[150rem] mx-auto">
-                <Card className="bg-card border-[var(--button-tournament-secondary)] border-[0.5px] flex flex-col ">
+                <MatchCard className="flex flex-col overflow-x-auto">
 
                     <div className="inset-0 right-2 text-muted-text ml-4 -mt-2 text-[1rem]">#tournamentid</div>
                     
@@ -20,14 +19,14 @@ const TournamentsWaiting = () => {
                         <h1 className="font-font font-semibold text-[2.5rem] ml-5 -mt-3">Tournament Title</h1>
 
                         <div className="gap-5 flex flex-row ml-auto mr-6 -mt-5">
-                            <MatchCard className="w-[60%] min-h-[40%] hover:opacity-90 hover:scale-105 transition-transform duration-300">
+                            <MatchCard className="w-[60%] h-[70%] hover:opacity-90 hover:scale-105 transition-transform duration-300">
                                 <div className="flex flex-row gap-2">
                                     <LogOut size={20} className="text-muted-text ml-3 -mt-2.5"/>
                                     <h2 className="font-font font-semibold text-[0.9rem] text-muted-text -ml-3 px-3 -mt-2.5">Leave Waiting Room</h2>
                                 </div>
                             </MatchCard>
-                            <TournamentButton className="items-center h-[67%] justify-center px-2 py-2 rounded-sm">
-                                <div className="flex flex-row my-auto">
+                            <TournamentButton className="items-center h-[80%] min-w-40 justify-center px-2 rounded-sm">
+                                <div className="flex flex-row mt-2.5">
                                     <Rocket size={25} className="ml-1"/>
                                     <div className="font-font font-semibold text-[1.1rem] ml-2 mr-1">Start Match</div>
                                 </div>
@@ -37,17 +36,17 @@ const TournamentsWaiting = () => {
 
                     <div className="flex flex-col -mt-1">
                         <div className="flex flex-row">
-                            <div className="font-font font-semibold text-[1.2rem] text-primary ml-6">Room Capacity</div>
-                            <div className="ml-auto font-semibold mr-6 text-[1.1rem]">4/8 Players</div>
+                            <div className="font-font font-semibold text-[1rem] text-primary ml-6">Room Capacity</div>
+                            <div className="ml-auto font-semibold mr-6 text-[0.9rem] -mb-3">4/8 Players</div>
                         </div>
-                        <Progress value={50} className="w-[95%] mx-auto mt-2"/>
+                        <Progress value={50} className="w-[96%] mx-auto mt-2"/>
                     </div>
 
                     <div className="flex flex-row">
-                        <Timer size={35} className="text-muted-text ml-5"/>
-                        <div className="font-font font-semibold text-xs mt-3 ml-2">Time Until Start:</div>
+                        <Timer size={30} className="text-muted-text ml-5 -mt-3"/>
+                        <div className="font-font font-semibold text-xs -mt-1 ml-2">Time Until Start:</div>
                     </div>
-                </Card>
+                </MatchCard>
 
                 <div className="flex flex-row mt-10 ml-5">
                     <h1 className="font-semibold text-xl">Players:</h1>
@@ -67,8 +66,8 @@ const TournamentsWaiting = () => {
                     </MatchCard>
                 </div>
 
-                <TournamentButton className="mt-10 h-15 w-[30rem] mx-auto justify-center">
-                    <h1 className="font-semibold my-auto mx-auto text-md">Click to Show You Are Ready!</h1>
+                <TournamentButton className="mt-15 mb-15 h-auto w-auto mx-auto justify-center rounded-sm px-2">
+                    <h1 className="font-semibold my-auto mx-auto text-sm">Click to Show You Are Ready!</h1>
                 </TournamentButton>
 
                 <div className="">
