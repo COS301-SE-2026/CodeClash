b8fb848f24254b0a0248f1cfe777c36e4465160c
TaskeenAbdoola <u22698681@tuks.co.za>
2026-09-22 18:17:55 +0200

    moved loaction

--- frontend/src/assets/Shop/index.ts
+++ frontend/src/assets/Shop/index.ts
@@ -1,9 +1,47 @@
-import Atlas from './Atlas.png';
-import Axiom from './Axiom.png';
-import Brix from './Brix.png';
-import Kairo from './Kairo.png';
-import Nyx from './Nyx.png';
-import Riven from './Riven.png';
-import Vexa from './Vexa.png';
-import Xara from './Xara.png';
-import Zen from './Zen.png';
+import Atlas from './Avatars/Atlas.png';
+import Axiom from './Avatars/Axiom.png';
+import Brix from './Avatars/Brix.png';
+import Kairo from './Avatars/Kairo.png';
+import Nyx from './Avatars/Nyx.png';
+import Riven from './Avatars/Riven.png';
+import Vexa from './Avatars/Vexa.png';
+import Xara from './Avatars/Xara.png';
+import Zen from './Avatars/Zen.png';
+
+import AbyssalCrown from './Accessories/Headwear/AbyssalCrown.png';
+import CodeCap from './Accessories/Headwear/CodeCap.png';
+import FrostEars from './Accessories/Headwear/FrostEars.png';
+import LumiEars from './Accessories/Headwear/LumiEars.png';
+import NeonEars from './Accessories/Headwear/NeonEars.png';
+import ScholarsCap from './Accessories/Headwear/ScholarsCap.png';
+import SignalHood from './Accessories/Headwear/SignalHood.png';
+import VerdantAntannae from './Accessories/Headwear/VerdantAntannae.png';
+import VoidCaster from './Accessories/Headwear/VoidCaster.png';
+
+import AzureScarf from './Accessories/Neckwear/AzureScarf.png';
+import CrimsonScarf from './Accessories/Neckwear/CrimsonScarf.png';
+import PolarScarf from './Accessories/Neckwear/PolarScarf.png';
+import SignalScarf from './Accessories/Neckwear/SignalScarf.png';
+import StarlightScarf from './Accessories/Neckwear/StarlightScarf.png';
+import TideScarf from './Accessories/Neckwear/TideScarf.png';
+
+import BlueVisor from './Accessories/Facewear/BlueVisor.png';
+import OrbitShades from './Accessories/Facewear/OrbitShades.png';
+import PinkVisor from './Accessories/Facewear/PinkVisor.png';
+import VerdantFrames from './Accessories/Facewear/VerdantFrames.png';
+import VoidFrames from './Accessories/Facewear/VoidFrames.png';
+
+import GuardianBelt from './Accessories/Belts/GuardianBelt.png';
+import RunnerBelt from './Accessories/Belts/RunnerBelt.png';
+import ShadowHarness from './Accessories/Belts/ShadowHarness.png';
+import StarterBelt from './Accessories/Belts/StarterBelt.png';
+import TideguardBelt from './Accessories/Belts/TideguardBelt.png';
+
+import AquaVeil from './Accessories/Capes/AquaVeil.png';
+import BlueOrbit from './Accessories/Capes/BlueOrbit.png';
+import EclipseShroud from './Accessories/Capes/EclipseShroud.png';
+import PinkComet from './Accessories/Capes/PinkComet.png';
+import SolarEdge from './Accessories/Capes/SolarEdge.png';
+import Starlance from './Accessories/Capes/Starlance.png';
+import TideRunner from './Accessories/Capes/TideRunner.png';
+import VoidMantle from './Accessories/Capes/VoidMantle.png';
