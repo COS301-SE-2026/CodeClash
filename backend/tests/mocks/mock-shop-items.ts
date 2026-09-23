b82c882cc1a97ead92358df668fbab17dccd76c1
nsmzoneli <nsmzoneli@gmail.com>
2026-09-23 21:38:06 +0200

    added the skeletal structure of mock shop items file

---
+++ backend/tests/mocks/mock-shop-items.ts
@@ -0,0 +1,36 @@
+import { ShopItem } from '../../src/entities/database/shop-item.entities';
+
+export const mock_shop_items: Partial<ShopItem>[] = [
+    {
+        category:
+        name: 
+        description:
+        price:
+        rarity:
+        metadata: {}
+    },
+    {
+        category:
+        name: 
+        description:
+        price:
+        rarity:
+        metadata: {}
+    },
+    {
+        category:
+        name: 
+        description:
+        price:
+        rarity:
+        metadata: {}
+    },
+    {
+        category:
+        name: 
+        description:
+        price:
+        rarity:
+        metadata: {}
+    }
+];
