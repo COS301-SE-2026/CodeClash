a8a52befd883d34c6c5eff96523d24acb9cf1b94
nsmzoneli <nsmzoneli@gmail.com>
2026-09-24 01:39:19 +0200

    added the implementation for the last few tests in the purchase service test file

--- backend/tests/integration/services/purchase.service.test.ts
+++ backend/tests/integration/services/purchase.service.test.ts
@@ -71,10 +71,17 @@ describe('Tests PurchaseService', () => {
     });
 
     it('Throws and does not deduct balance when balance is insufficient', async () => {
+        const item_id = item_ids.find((_, i) => mock_shop_items[i]!.category === 'theme' && mock_shop_items[i]);
 
+        await expect(purchase_service.purchaseItem(user_id, item_id!)).rejects.toThrow('Insufficient balance');
+
+        const wallet = await wallet_repo.getWallet(user_id);
+        expect(wallet!.balance).toBe(400);
     });
 
     it('Throws when item does not exist', async () => {
-
+        await expect(
+            purchase_service.purchaseItem(user_id, randomUUID())
+        ).rejects.toThrow('Item not found');
     });
 });
