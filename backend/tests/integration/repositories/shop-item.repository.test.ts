e3c0d32af9d7cbd1e7382d8b22b0fa89ba1cf578
nsmzoneli <nsmzoneli@gmail.com>
2026-09-24 10:09:40 +0200

    added implementation for before and after of shop item repo test

--- backend/tests/integration/repositories/shop-item.repository.test.ts
+++ backend/tests/integration/repositories/shop-item.repository.test.ts
@@ -1,25 +1,30 @@
-import { describe, expect, it, beforeAll, afterAll } from "vitest";
+import { describe, expect, it, beforeAll, afterAll, should } from "vitest";
 import { DataSource } from "typeorm";
 import { createTestDataSource } from "../../test-data-source";
 import { ShopItem } from "../../../src/entities/database/shop-item.entities";
 import { ShopItemRepository } from "../../../src/interface-adapters/repositories/shop-item.repository";
 import { mock_shop_items } from "../../mocks/mock-shop-items";
+import { create } from "node:domain";
 
 let data_source: DataSource;
 let repo: ShopItemRepository;
-let create_ids: string[] = [];
+let created_ids: string[] = [];
 
 describe('Tests ShopItemRepositor', () => {
     beforeAll(async () => {
+        data_source = await createTestDataSource();
+        repo = new ShopItemRepository(data_source.getRepository(ShopItem));
 
+        const saved  = await data_source.getRepository(ShopItem).save(mock_shop_items);
+        created_ids = saved.map(i => i.shop_item_id);
     });
 
     afterAll(async () => {
-
+        await data_source.getRepository(ShopItem).delete(created_ids);
     });
 
     it('Returns all shop items', async () => {
-
+        
     });
 
     it('Correctly maps category and metadata', async () => {
