c49586f09882bdcfec5593cc4eaae0e086b3c99f
nsmzoneli <nsmzoneli@gmail.com>
2026-09-25 06:59:32 +0200

    removed accessories from schema

--- database/01-init.sql
+++ database/01-init.sql
@@ -190,11 +190,6 @@ CREATE TABLE IF NOT EXISTS equipped_items (
   equipped_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   user_id UUID REFERENCES users(user_id),
   avatar_item_id UUID REFERENCES shop_items(shop_item_id),
-  headwear_id UUID REFERENCES shop_items(shop_item_id),
-  neckwear_id UUID REFERENCES shop_items(shop_item_id),
-  facewear_id UUID REFERENCES shop_items(shop_item_id),
-  belt_id REFERENCES shop_items(shop_item_id),
-  one_piece_id REFERENCES shop_items(shop_item_id),
   powerup_item_id UUID REFERENCES shop_items(shop_item_id),
   theme_id UUID REFERENCES shop_items(shop_item_id),
   updated_at TIMESTAMP DEFAULT NOW()
