e97597405fbb85bab937d23fb07c4a56cb621c22
morganthegirlboss <u24910882@tuks.co.za>
2026-09-25 17:13:02 +0200

    adding selected case to multiple choice component

--- frontend/@/components/ui/MultipleChoice.tsx
+++ frontend/@/components/ui/MultipleChoice.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 
 import "../../../src/styles/global.css"
 import { MatchCard } from './MatchCard'
+import {Check} from "lucide-react"
 
 
 interface MultipleChoiceProps{
@@ -12,19 +13,29 @@ interface MultipleChoiceProps{
     className?: string;
     letter?: string;
     option?: string;
+    selected?: boolean;
 }
 
-export const MultipleChoice = ({children, className, letter, option} : MultipleChoiceProps) => {
+export const MultipleChoice = ({children, className, letter, option, selected} : MultipleChoiceProps) => {
 
     return(
-        <MatchCard className={`bg-[var(--multiple-choice-box)] flex flex-row w-auto overflow-x-auto gap-1 min-w-[10%] ${className}`}>
+        <MatchCard className={`bg-[var(--multiple-choice-box)] flex flex-row w-auto overflow-x-auto gap-1 
+        min-w-[10%] ${selected ? 'border-primary shadow-[0_0_12px_(--var-primary)]': ""} ${className}`}>
             
-            <div className="bg-[var(--multiple-choice-letter)] border-[0.5px] my-auto sm:min-w-0 
-                border-[var(--button-tournament-secondary-border)] rounded-[10px] ml-2 w-10 h-10 text-center"
+            <div className={`bg-[var(--multiple-choice-letter)] border-[0.5px] my-auto sm:min-w-0 
+                border-[var(--button-tournament-secondary-border)] rounded-[10px] ml-2 w-10 h-10 text-center
+                ${selected ? "bg-primary shadow-[0_0_12px_(--var-primary)" : ""}`}
             >
                 <h1 className="text-secondary text-[15px] my-auto mt-1.75">{letter}</h1> 
             </div>
+
             <h1 className="font-semibold text-sm ml-3 mt-1">{option}</h1>
+
+            <MatchCard className={`p-0 flex rounded-full ml-auto h-6 w-6 bg-[var(--multiple-choice-box)] 
+                border-[var(--button-tournament-secondary-border)] mr-3 my-auto items-center justify-center
+                ${selected ? "bg-primary shadow-[0_0_12px_(--var-primary)" : ""}`}>
+                        
+            </MatchCard>
             {children}
         </MatchCard>
     )
