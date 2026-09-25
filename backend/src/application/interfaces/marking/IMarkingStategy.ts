9001060751a77ee76240ed9d4fec671db219347b
Ntuuu <apathylely@gmail.com>
2026-09-25 16:57:20 +0200

    Changed: slightl changes to the marking stratey interface, to make use of an outcome based on correctness runtime and memory usage gotten from judge 0

--- backend/src/application/interfaces/marking/IMarkingStategy.ts
+++ backend/src/application/interfaces/marking/IMarkingStategy.ts
@@ -1,6 +1,13 @@
 import { MathsSubmissionDTO, ProgSubmissionDTO } from "src/entities/dtos/submissions/submission.dto";
 import { AnswerDTO } from "../../../entities/dtos/questions/answer.dto";
 
+export interface MarkOutcome {
+    correct: boolean;
+    run_time_ms?: number | null;
+    memory_kb?: number | null;
+    
+}
+
 export interface IMarkingStrategy{
-    mark(submission: MathsSubmissionDTO | ProgSubmissionDTO, answer: AnswerDTO): Promise<boolean>;
+    mark(submission: MathsSubmissionDTO | ProgSubmissionDTO, answer: AnswerDTO): Promise<MarkOutcome>;
 }
