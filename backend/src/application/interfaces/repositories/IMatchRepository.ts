6a05913eea6cd97f76da6a7f17a7c7b28dd5cbcc
Ntuuu <apathylely@gmail.com>
2026-09-26 00:37:42 +0200

    Added: skill progress to the match repositories

--- backend/src/application/interfaces/repositories/IMatchRepository.ts
+++ backend/src/application/interfaces/repositories/IMatchRepository.ts
@@ -1,11 +1,14 @@
 import { MatchMode, MatchPlayer, MatchStatus, MatchType } from "src/entities/dtos/matches/match.dto";
 import { MatchHistoryRow } from "src/entities/dtos/matches/match.dto";
 import { MatchResultDTO } from "src/entities/dtos/matches/match.dto";
+import { SkillProgressGame } from "src/entities/dtos/matches/match.dto";
+
 
 export interface IMatchRepository {
     createMatch(players: string[], match_type: MatchType, match_mode: MatchMode, match_start: Date): Promise<string>,
     completeMatch(match_id: string, status: MatchStatus): Promise<void>,
     updatePlayers(match_id: string, players: MatchPlayer[]): Promise<void>,
     getMatchHistory(user_id: string): Promise<MatchHistoryRow[]>,
-    buildMatchResult(match_id: string): Promise<MatchResultDTO>
+    buildMatchResult(match_id: string): Promise<MatchResultDTO>,
+    getSkillProgress(user_id: string): Promise<SkillProgressGame[]>
 }
